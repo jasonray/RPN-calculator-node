80af2db4e2d6e204d2d5a4f378f4ec0e076bcfee
Jason Ray <jason.ray@agilex.com>
2014-03-09 17:42:42 -0400

    added absolute operator

--- operator-registry.js
+++ operator-registry.js
@@ -3,6 +3,7 @@ var rpnstack = require("./rpn-stack");
 var registry = new Array();
 registry.push( require("./operators/addition-operator") );
 registry.push( require("./operators/subtraction-operator") );
+registry.push( require("./operators/absolute-operator") );
 
 function getOperator(operatorCharacter) {
 	console.log("finding operator for " + operatorCharacter + "; current registry has " + registry);
