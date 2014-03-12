09eca8dc3dac474f62b91101434537f817ef36e2
Jason Ray <jason.ray@agilex.com>
2014-03-12 19:12:10 -0400

    add sum operator

---
+++ operators/sum-operator.js
@@ -0,0 +1,14 @@
+var rpnstack = require("../rpn-stack");
+
+function doOperation(numbers) {
+	var sum = 0;
+
+	while (!numbers.isEmpty()) {
+		sum=sum+numbers.pop();
+	}
+
+	numbers.push(sum);
+	return sum;
+}
+
+exports.doOperation=doOperation;
