1527d4b3d547ddaeb50e8e22cc8f9fb8dfeec4f6
Jason Ray <jason.ray@agilex.com>
2014-05-08 07:52:36 -0400

    formatting

--- test/rpn-stack-test.js
+++ test/rpn-stack-test.js
@@ -1,54 +1,54 @@
 var rpnstack = require('../rpn-stack');
 
-exports.popEmptyStack = function (test) {
+exports.popEmptyStack = function(test) {
 	var stack = new rpnstack.stack();
 	var result = stack.pop();
-	test.equals(0,result);
+	test.equals(0, result);
 	test.done();
 };
 
-exports.peekEmptyStack = function (test) {
+exports.peekEmptyStack = function(test) {
 	var stack = new rpnstack.stack();
-	test.equals(0,stack.peek());
+	test.equals(0, stack.peek());
 	test.done();
 };
 
-exports.peekFromPush = function (test) {
-	var stack =  new rpnstack.stack();
+exports.peekFromPush = function(test) {
+	var stack = new rpnstack.stack();
 	stack.push(2);
-	test.equals(2,stack.peek());
+	test.equals(2, stack.peek());
 	test.done();
 };
 
-exports.popFromPush = function (test) {
-	var stack =  new rpnstack.stack();
-	stack.push(2);
-	stack.push(5);
-	test.equals(5,stack.pop());
-	test.equals(2,stack.pop());
-	test.done();
-};
-
-exports.multiplePeeks = function (test) {
-	var stack =  new rpnstack.stack();
+exports.popFromPush = function(test) {
+	var stack = new rpnstack.stack();
 	stack.push(2);
 	stack.push(5);
-	test.equals(5,stack.peek());
-	test.equals(5,stack.peek());
+	test.equals(5, stack.pop());
+	test.equals(2, stack.pop());
 	test.done();
 };
 
-exports.emptyStackWithPopsNextReturns0 = function (test) {
-	var stack =  new rpnstack.stack();
+exports.multiplePeeks = function(test) {
+	var stack = new rpnstack.stack();
 	stack.push(2);
-	test.equals(2,stack.pop());
-	test.equals(0,stack.peek());
+	stack.push(5);
+	test.equals(5, stack.peek());
+	test.equals(5, stack.peek());
+	test.done();
+};
+
+exports.emptyStackWithPopsNextReturns0 = function(test) {
+	var stack = new rpnstack.stack();
+	stack.push(2);
+	test.equals(2, stack.pop());
+	test.equals(0, stack.peek());
 	test.done();
 };
 
 exports.pushPop = function(test) {
-	var stack =  new rpnstack.stack();
+	var stack = new rpnstack.stack();
 	stack.push(1);
-	test.equals(1,stack.pop());
+	test.equals(1, stack.pop());
 	test.done();
 }
