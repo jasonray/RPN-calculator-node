8bc352ab95ff3f7892401c22c35d683bcfdcf3d3
Jason Ray <jason.ray@agilex.com>
2014-05-08 07:31:52 -0400

    formatting

--- test/absolute-test.js
+++ test/absolute-test.js
@@ -1,51 +1,51 @@
 var calculator = require('../calculator');
 
-exports.empty = function (test) {
+exports.empty = function(test) {
 	var calc = new calculator.calc();
-	test.equals(0,calc.perform("||"));
+	test.equals(0, calc.perform("||"));
 	test.done();
 };
 
-exports.zero = function (test) {
+exports.zero = function(test) {
 	var calc = new calculator.calc();
 	calc.enter(0);
-	test.equals(0,calc.perform("||"));
+	test.equals(0, calc.perform("||"));
 	test.done();
 };
 
-exports.one = function (test) {
+exports.one = function(test) {
 	var calc = new calculator.calc();
 	calc.enter(1);
-	test.equals(1,calc.perform("||"));
+	test.equals(1, calc.perform("||"));
 	test.done();
 };
 
-exports.negOne = function (test) {
+exports.negOne = function(test) {
 	var calc = new calculator.calc();
 	calc.enter(-1);
-	test.equals(1,calc.perform("||"));
+	test.equals(1, calc.perform("||"));
 	test.done();
 };
 
-exports.negTen = function (test) {
+exports.negTen = function(test) {
 	var calc = new calculator.calc();
 	calc.enter(-10);
-	test.equals(10,calc.perform("||"));
+	test.equals(10, calc.perform("||"));
 	test.done();
 };
 
-exports.ensureResultIsOnStack = function (test) {
+exports.ensureResultIsOnStack = function(test) {
 	var calc = new calculator.calc();
 	calc.enter(1);
 	calc.enter(-5);
 	calc.perform("||")
-	test.equals(6,calc.perform("+"));
+	test.equals(6, calc.perform("+"));
 	test.done();
 };
 
-exports.decimal = function (test) {
+exports.decimal = function(test) {
 	var calc = new calculator.calc();
 	calc.enter(-1.5);
-	test.equals(1.5,calc.perform("||"));
+	test.equals(1.5, calc.perform("||"));
 	test.done();
 };
