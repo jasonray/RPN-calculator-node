448d4b08687bdffa0d8cf5efb268269a0bbdc414
Jason Ray <jason.ray@agilex.com>
2014-05-08 07:52:48 -0400

    formatting

--- test/subtraction-test.js
+++ test/subtraction-test.js
@@ -1,46 +1,45 @@
 var calculator = require('../calculator');
 
-exports.subtractNoNumbers = function (test) {
+exports.subtractNoNumbers = function(test) {
 	var calc = new calculator.calc();
 	var result = calc.perform("-");
-	test.equals(0,result);
+	test.equals(0, result);
 	test.done();
 };
 
-exports.subtractOneNumber = function (test) {
+exports.subtractOneNumber = function(test) {
 	var calc = new calculator.calc();
 	calc.enter(1);
 	var result = calc.perform("-");
-	test.equals(-1,result);
+	test.equals(-1, result);
 	test.done();
 };
 
-exports.subtractOneNumberTwice = function (test) {
+exports.subtractOneNumberTwice = function(test) {
 	var calc = new calculator.calc();
 	calc.enter(1);
 	calc.perform("-");
 	var result = calc.perform("-");
-	test.equals(1,result);
+	test.equals(1, result);
 	test.done();
 };
 
-exports.subtractTwoNumbers = function (test) {
+exports.subtractTwoNumbers = function(test) {
 	var calc = new calculator.calc();
 	calc.enter(10);
 	calc.enter(3);
 	var result = calc.perform("-");
-	test.equals(7,result);
+	test.equals(7, result);
 	test.done();
 };
 
-exports.subtractThreeNumbers = function (test) {
+exports.subtractThreeNumbers = function(test) {
 	var calc = new calculator.calc();
 	calc.enter(21);
 	calc.enter(13);
 	calc.perform("-");
 	calc.enter(5);
 	var result = calc.perform("-");
-	test.equals(3,result);
+	test.equals(3, result);
 	test.done();
 };
-
