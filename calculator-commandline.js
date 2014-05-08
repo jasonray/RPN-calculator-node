f40d1e34f52f98cb79eedab99e86ee5e8096b244
Jason Ray <jason.ray@agilex.com>
2014-05-08 07:23:47 -0400

    formatting / address jslint findings

--- calculator-commandline.js
+++ calculator-commandline.js
@@ -1,19 +1,19 @@
 var calculator = require('./calculator');
 
 console.log("result: " + processCommandlineArgs(new calculator.calc()));
+
 function processCommandlineArgs(calc) {
-	process.argv.forEach(function (val, index, array) {
-		if (index==0) {
+	process.argv.forEach(function(val, index, array) {
+		if (index === 0) {
 			//this is likely 'node'
-		}
-		else if (index==1) {
+		} else if (index === 1) {
 			//this is likely 'calculator.js'
 		} else if (isNaN(val)) {
 			// console.log("operator:" + val);
-			result=calc.perform(val);
+			result = calc.perform(val);
 		} else {
 			// console.log("operand:" + val);
-			calc.enter(parseInt(val));
+			calc.enter(parseInt(val, 10));
 		}
 	});
 	return result;
