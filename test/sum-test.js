83ffe4a2300a630ab8acadc623ee067fd9d4e4e1
Jason Ray <jason.ray@agilex.com>
2014-05-08 07:53:54 -0400

    formatting

--- test/sum-test.js
+++ test/sum-test.js
@@ -1,42 +1,41 @@
 var calculator = require('../calculator');
 
-exports.sumNoNumbers = function (test) {
+exports.sumNoNumbers = function(test) {
 	var calc = new calculator.calc();
-	test.equals(0,calc.perform("sum"));
+	test.equals(0, calc.perform("sum"));
 	test.done();
 };
 
-exports.sumOneNumber = function (test) {
+exports.sumOneNumber = function(test) {
 	var calc = new calculator.calc();
 	calc.enter(5);
-	test.equals(5,calc.perform("sum"));
+	test.equals(5, calc.perform("sum"));
 	test.done();
 };
 
-exports.sumTwoNumbers = function (test) {
+exports.sumTwoNumbers = function(test) {
 	var calc = new calculator.calc();
 	calc.enter(5);
 	calc.enter(3);
-	test.equals(8,calc.perform("sum"));
+	test.equals(8, calc.perform("sum"));
 	test.done();
 };
 
-exports.sumThreeNumbers = function (test) {
+exports.sumThreeNumbers = function(test) {
 	var calc = new calculator.calc();
 	calc.enter(5);
 	calc.enter(3);
 	calc.enter(1);
-	test.equals(9,calc.perform("sum"));
+	test.equals(9, calc.perform("sum"));
 	test.done();
 };
 
-exports.subtractThreeNumbers = function (test) {
+exports.subtractThreeNumbers = function(test) {
 	var calc = new calculator.calc();
 	calc.enter(11);
 	calc.enter(5);
 	calc.enter(3);
 	calc.enter(1);
-	test.equals(20,calc.perform("sum"));
+	test.equals(20, calc.perform("sum"));
 	test.done();
 };
-
