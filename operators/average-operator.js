ec7f4bd28096e2ad40e7cf301a70314963f9d32a
Jason Ray <jason.ray@agilex.com>
2014-05-08 22:14:28 -0400

    formatting

--- operators/average-operator.js
+++ operators/average-operator.js
@@ -1,17 +1,17 @@
-exports.doOperation=function (numbers) {
+exports.doOperation = function(numbers) {
 	var count = 0;
 	var sum = 0;
 
 	while (!numbers.isEmpty()) {
-		count=count+1;
-		sum=sum+numbers.pop();
+		count = count + 1;
+		sum = sum + numbers.pop();
 	}
 
 	var result;
-	if (count===0) {
-		result=0;
+	if (count === 0) {
+		result = 0;
 	} else {
-		result = sum/count;
+		result = sum / count;
 	}
 
 	numbers.push(result);
