f4e1f004668f4b252515f7cceaa334af1289c932
Jason Ray <jason.ray@agilex.com>
2014-05-08 22:18:43 -0400

    formatting

--- operators/multiplication-operator.js
+++ operators/multiplication-operator.js
@@ -1,9 +1,7 @@
-function doOperation(numbers) {
+exports.doOperation = function doOperation(numbers) {
 	var rhs = numbers.pop();
 	var lhs = numbers.pop();
 	var result = lhs * rhs;
 	numbers.push(result);
 	return result;
-}
-
-exports.doOperation = doOperation;
+};
