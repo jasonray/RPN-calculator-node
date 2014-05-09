1d710ddeb7eb40ac1d90bdab525e0a279bc75bcc
Jason Ray <jason.ray@agilex.com>
2014-05-08 22:11:48 -0400

    simplify exports

--- operators/absolute-operator.js
+++ operators/absolute-operator.js
@@ -1,12 +1,10 @@
 var rpnstack = require("../rpn-stack");
 
-function doOperation(numbers) {
+exports.doOperation = function(numbers) {
 	var n = numbers.pop();
-	if (n<0) {
-		n=n*-1;
+	if (n < 0) {
+		n = n * -1;
 	}
 	numbers.push(n);
 	return n;
-}
-
-exports.doOperation=doOperation;
+};
