7f5831395311ff2464b98daac16f515efb1ad071
jason ray <jasonray@jayray.net>
2014-10-30 17:36:02 -0400

    modifed test to handle new constructor

--- test/enter-test.js
+++ test/enter-test.js
@@ -1,19 +1,19 @@
 var Calculator = require('../calculator');
 
 exports.enterStringReturnsValue = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	test.equals(1, calc.enter("1"));
 	test.done();
 };
 
 exports.enterNumberReturnsValue = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	test.equals(1, calc.enter(1));
 	test.done();
 };
 
 exports.enterNonInteger = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	test.equals(1.5, calc.enter("1.5"));
 	test.done();
 };
