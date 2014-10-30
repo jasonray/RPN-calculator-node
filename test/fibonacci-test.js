1da38d32ca04a88a0660d84805ab0f114848910c
jason ray <jasonray@jayray.net>
2014-10-30 17:35:02 -0400

    modifed test to handle new constructor

--- test/fibonacci-test.js
+++ test/fibonacci-test.js
@@ -1,83 +1,83 @@
-var calculator = require('../calculator');
+var Calculator = require('../calculator');
 
 exports.fibEmpty = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	test.equals(0, calc.perform("fib"));
 	test.done();
 };
 
 exports.fib1 = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(1);
 	test.equals(1, calc.perform("fib"));
 	test.done();
 };
 
 exports.fib2 = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(2);
 	test.equals(1, calc.perform("fib"));
 	test.done();
 };
 
 exports.fib3 = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(3);
 	test.equals(2, calc.perform("fib"));
 	test.done();
 };
 
 exports.fib4 = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(4);
 	test.equals(3, calc.perform("fib"));
 	test.done();
 };
 
 exports.fib5 = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(5);
 	test.equals(5, calc.perform("fib"));
 	test.done();
 };
 
 exports.fib6 = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(6);
 	test.equals(8, calc.perform("fib"));
 	test.done();
 };
 
 exports.fib7 = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(7);
 	test.equals(13, calc.perform("fib"));
 	test.done();
 };
 
 exports.fib8 = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(8);
 	test.equals(21, calc.perform("fib"));
 	test.done();
 };
 
 exports.fib30 = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(30);
 	test.equals(832040, calc.perform("fib"));
 	test.done();
 };
 
 exports.fib40 = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(40);
 	test.equals(102334155, calc.perform("fib"));
 	test.done();
 };
 
 exports.nonInteger = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(1.1);
 	test.throws(function() {
 		calc.perform("fib");
