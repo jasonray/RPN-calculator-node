a33c07281ec29acdb6bec1a856600724ac305c4e
jason ray <jasonray@jayray.net>
2014-10-30 17:35:31 -0400

    modifed test to handle new constructor

--- test/factorial-test.js
+++ test/factorial-test.js
@@ -1,20 +1,20 @@
-var calculator = require('../calculator');
+var Calculator = require('../calculator');
 
 exports.factorialReturnsProduct = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(4);
 	test.equals(24, calc.perform("!"));
 	test.done();
 };
 
 exports.factorialOnNothing = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	test.equals(1, calc.perform("!"));
 	test.done();
 };
 
 exports.twoFactorials = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(3);
 	calc.perform("!");
 	test.equals(720, calc.perform("!"));
@@ -22,7 +22,7 @@ exports.twoFactorials = function(test) {
 };
 
 exports.nonInteger = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(3.1);
 
 	test.throws(function() {
