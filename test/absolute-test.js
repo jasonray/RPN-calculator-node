03acc9552d610af70aa93c91ab72d0ac0d63b2a4
jason ray <jasonray@jayray.net>
2014-10-30 17:38:06 -0400

    modifed test to handle new constructor

--- test/absolute-test.js
+++ test/absolute-test.js
@@ -1,41 +1,41 @@
-var calculator = require('../calculator');
+var Calculator = require('../calculator');
 
 exports.empty = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	test.equals(0, calc.perform("||"));
 	test.done();
 };
 
 exports.zero = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(0);
 	test.equals(0, calc.perform("||"));
 	test.done();
 };
 
 exports.one = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(1);
 	test.equals(1, calc.perform("||"));
 	test.done();
 };
 
 exports.negOne = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(-1);
 	test.equals(1, calc.perform("||"));
 	test.done();
 };
 
 exports.negTen = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(-10);
 	test.equals(10, calc.perform("||"));
 	test.done();
 };
 
 exports.ensureResultIsOnStack = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(1);
 	calc.enter(-5);
 	calc.perform("||")
@@ -44,7 +44,7 @@ exports.ensureResultIsOnStack = function(test) {
 };
 
 exports.decimal = function(test) {
-	var calc = new calculator.calc();
+	var calc = new Calculator();
 	calc.enter(-1.5);
 	test.equals(1.5, calc.perform("||"));
 	test.done();
