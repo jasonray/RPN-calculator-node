0aaa6cb9eab1e9c7c48130d591e445b7b2a8d5a8
jason ray <jasonray@jayray.net>
2014-10-30 20:38:40 -0400

    move stuff to prototype

--- rpn-stack.js
+++ rpn-stack.js
@@ -2,13 +2,6 @@ function Stack() {
 	var self = this;
 	self.internalStack = [];
 
-	self.pop = function() {
-		if (this.isEmpty()) {
-			return 0;
-		} else {
-			return self.internalStack.pop();
-		}
-	};
 
 
 
@@ -36,5 +29,15 @@ function Stack() {
 			return self.internalStack[self.internalStack.length - 1];
 		}
 	};
+
+		Stack.prototype.pop = function() {
+			var self = this;
+		if (self.isEmpty()) {
+			return 0;
+		} else {
+			return self.internalStack.pop();
+		}
+	};
+
 
 module.exports = Stack;
