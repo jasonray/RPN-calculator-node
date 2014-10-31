39069740321496b9a3ba1eabe34f83a3c20750f5
jason ray <jasonray@jayray.net>
2014-10-30 20:35:58 -0400

    change constructor

--- rpn-stack.js
+++ rpn-stack.js
@@ -1,31 +1,32 @@
 function Stack() {
-	internalStack = [];
+	var self = this;
+	self.internalStack = [];
 
-	this.push = function(n) {
-		internalStack.push(n);
+	self.push = function(n) {
+		self.internalStack.push(n);
 	};
 
-	this.pop = function() {
+	self.pop = function() {
 		if (this.isEmpty()) {
 			return 0;
 		} else {
-			return internalStack.pop();
+			return self.internalStack.pop();
 		}
 	};
 
-	this.peek = function() {
+	self.peek = function() {
 		if (this.isEmpty()) {
 			return 0;
 		} else {
-			return internalStack[internalStack.length - 1];
+			return self.internalStack[self.internalStack.length - 1];
 		}
 	};
 
-	this.isEmpty = function() {
-		return (internalStack.length === 0);
+	self.isEmpty = function() {
+		return (self.internalStack.length === 0);
 	};
 
-	return this;
+	return self;
 
 }
 
