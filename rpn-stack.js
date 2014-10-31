af76de8bbffb2d3a0e86c6a0d5299a615a34a133
jason ray <jasonray@jayray.net>
2014-10-30 20:45:20 -0400

    formatting

--- rpn-stack.js
+++ rpn-stack.js
@@ -4,33 +4,33 @@ function Stack() {
 	return self;
 }
 
-	Stack.prototype.push = function(n) {
-		var self = this;
-		self.internalStack.push(n);
-	};
+Stack.prototype.push = function(n) {
+	var self = this;
+	self.internalStack.push(n);
+};
 
-		Stack.prototype.isEmpty = function() {
-			var self = this;
-		return (self.internalStack.length === 0);
-	};
+Stack.prototype.isEmpty = function() {
+	var self = this;
+	return (self.internalStack.length === 0);
+};
 
-	Stack.prototype.peek = function() {
-		var self = this;
-		if (self.isEmpty()) {
-			return 0;
-		} else {
-			return self.internalStack[self.internalStack.length - 1];
-		}
-	};
+Stack.prototype.peek = function() {
+	var self = this;
+	if (self.isEmpty()) {
+		return 0;
+	} else {
+		return self.internalStack[self.internalStack.length - 1];
+	}
+};
 
-		Stack.prototype.pop = function() {
-			var self = this;
-		if (self.isEmpty()) {
-			return 0;
-		} else {
-			return self.internalStack.pop();
-		}
-	};
+Stack.prototype.pop = function() {
+	var self = this;
+	if (self.isEmpty()) {
+		return 0;
+	} else {
+		return self.internalStack.pop();
+	}
+};
 
 
 module.exports = Stack;
