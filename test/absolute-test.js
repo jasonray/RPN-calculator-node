fc58bb6fae474e76b69558eab21c5e7a1e602bf1
jason ray <jasonray@jayray.net>
2014-10-30 21:22:51 -0400

    use module exports

--- test/absolute-test.js
+++ test/absolute-test.js
@@ -1,40 +1,40 @@
 var Calculator = require('../calculator');
 
-exports.empty = function(test) {
+module.exports.empty = function(test) {
 	var calc = new Calculator();
 	test.equals(0, calc.perform("||"));
 	test.done();
 };
 
-exports.zero = function(test) {
+module.exports.zero = function(test) {
 	var calc = new Calculator();
 	calc.enter(0);
 	test.equals(0, calc.perform("||"));
 	test.done();
 };
 
-exports.one = function(test) {
+module.exports.one = function(test) {
 	var calc = new Calculator();
 	calc.enter(1);
 	test.equals(1, calc.perform("||"));
 	test.done();
 };
 
-exports.negOne = function(test) {
+module.exports.negOne = function(test) {
 	var calc = new Calculator();
 	calc.enter(-1);
 	test.equals(1, calc.perform("||"));
 	test.done();
 };
 
-exports.negTen = function(test) {
+module.exports.negTen = function(test) {
 	var calc = new Calculator();
 	calc.enter(-10);
 	test.equals(10, calc.perform("||"));
 	test.done();
 };
 
-exports.ensureResultIsOnStack = function(test) {
+module.exports.ensureResultIsOnStack = function(test) {
 	var calc = new Calculator();
 	calc.enter(1);
 	calc.enter(-5);
@@ -43,7 +43,7 @@ exports.ensureResultIsOnStack = function(test) {
 	test.done();
 };
 
-exports.decimal = function(test) {
+module.exports.decimal = function(test) {
 	var calc = new Calculator();
 	calc.enter(-1.5);
 	test.equals(1.5, calc.perform("||"));
