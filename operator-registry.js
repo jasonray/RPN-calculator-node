2ecf5f46a8bbbedd61d8400c65017e851c088416
jason ray <jasonray@jayray.net>
2014-10-30 20:51:50 -0400

    formatting

--- operator-registry.js
+++ operator-registry.js
@@ -2,27 +2,27 @@
 
 function Registry() {
 	var self = this;
-	  self.handlers = {};
+	self.handlers = {};
 }
 
 Registry.prototype.registerHandler = function(operatorCharacter, operatorMethod) {
-		var self = this;
-		operatorCharacter = operatorCharacter.toLowerCase();
-		self.handlers[operatorCharacter] = operatorMethod;
-	};
+	var self = this;
+	operatorCharacter = operatorCharacter.toLowerCase();
+	self.handlers[operatorCharacter] = operatorMethod;
+};
 
-	Registry.prototype.getOperatorMethod = function(operatorCharacter) {
-		var self = this;
-		operatorCharacter = operatorCharacter.toLowerCase();
-		var handler = self.handlers[operatorCharacter];
-		if (typeof handler === 'function') {
-			return handler;
-		} else if (typeof handler === 'object') {
-			return handle[operatorCharacter].doOperation;
-		} else {
-			console.log("return null");
-			return null;
-		}
-	};
+Registry.prototype.getOperatorMethod = function(operatorCharacter) {
+	var self = this;
+	operatorCharacter = operatorCharacter.toLowerCase();
+	var handler = self.handlers[operatorCharacter];
+	if (typeof handler === 'function') {
+		return handler;
+	} else if (typeof handler === 'object') {
+		return handle[operatorCharacter].doOperation;
+	} else {
+		console.log("return null");
+		return null;
+	}
+};
 
 module.exports = Registry;
