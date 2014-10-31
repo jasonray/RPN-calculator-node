4479c19f779040717f21a1871fe406142348f06b
jason ray <jasonray@jayray.net>
2014-10-30 21:24:21 -0400

    use module exports

--- test/division-test.js
+++ test/division-test.js
@@ -1,6 +1,6 @@
 var Calculator = require('../calculator');
 
-exports.divideTwoNumber = function(test) {
+module.exports.divideTwoNumber = function(test) {
 	var calc = new Calculator();
 	calc.enter(8);
 	calc.enter(2);
