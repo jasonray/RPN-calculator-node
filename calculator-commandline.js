2504676d43f3146c168803eae3c51c31b031ac93
jason ray <jasonray@jayray.net>
2014-10-30 20:48:50 -0400

    formatting

--- calculator-commandline.js
+++ calculator-commandline.js
@@ -5,7 +5,7 @@ var Calculator = require('./calculator');
 console.log("result: " + processCommandlineArgs(new Calculator()));
 
 function processCommandlineArgs(calculator) {
-		var result=0;
+	var result = 0;
 	process.argv.forEach(function(val, index, array) {
 		if (index === 0) {
 			//this is likely 'node'
