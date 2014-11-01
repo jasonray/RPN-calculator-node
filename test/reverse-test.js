b522ae4210afab505ae8d77f657986c5d5a04b28
jason ray <jasonray@jayray.net>
2014-11-01 11:02:38 -0400

    support reverse 1 number

--- test/reverse-test.js
+++ test/reverse-test.js
@@ -17,3 +17,15 @@ module.exports.reverseOneNumber = function(test) {
 	test.equals(0, calc.numbers.pop());
 	test.done();
 };
+
+module.exports.reverseTwoNumbers = function(test) {
+	var calc = new Calculator();
+	calc.enter(1);
+	calc.enter(2);
+	var result = calc.perform("reverse");
+	test.equals(2, result);
+	test.equals(2, calc.numbers.pop());
+	test.equals(1, calc.numbers.pop());
+	test.equals(0, calc.numbers.pop());
+	test.done();
+};
