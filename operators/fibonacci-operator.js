844b86903c3cfdbcdec166cfa9e566162263d92c
Jason Ray <jason.ray@agilex.com>
2014-11-29 09:43:38 -0500

    formatting

--- operators/fibonacci-operator.js
+++ operators/fibonacci-operator.js
@@ -4,23 +4,23 @@
 var _ = require('underscore');
 
 module.exports = function(numbers) {
-	var n = numbers.pop();
+    var n = numbers.pop();
 
-	if (!isInteger(n)) {
-		throw new Error("Cannot perform fibonacci on non-integer");
-	}
+    if (!isInteger(n)) {
+        throw new Error("Cannot perform fibonacci on non-integer");
+    }
 
-	var result = fib(n);
+    var result = fib(n);
 
-	numbers.push(result);
-	return result;
+    numbers.push(result);
+    return result;
 
-	function fib(n) {
-		var prev1 = 1;
-		var prev2 = 0;
+    function fib(n) {
+        var prev1 = 1;
+        var prev2 = 0;
 
-		if (n <= 0)
-			return 0;
+        if (n <= 0)
+            return 0;
 
         _.times(n - 1, function() {
             var sum = prev1 + prev2;
@@ -28,10 +28,10 @@ module.exports = function(numbers) {
             prev1 = sum;
         });
 
-		return prev1;
-	}
+        return prev1;
+    }
 
-	function isInteger(n) {
-		return (n == parseInt(n, 10));
-	}
+    function isInteger(n) {
+        return (n == parseInt(n, 10));
+    }
 };
