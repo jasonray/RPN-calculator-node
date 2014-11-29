2cff8e553885046afe57e467e2b769310f464bda
Jason Ray <jason.ray@agilex.com>
2014-11-29 09:45:51 -0500

    formatting

--- test/clear-test.js
+++ test/clear-test.js
@@ -4,43 +4,43 @@
 var Calculator = require('../calculator');
 
 module.exports.clearEmpty = function(test) {
-	var calc = new Calculator();
-	calc.perform("C");
-	test.done();
+    var calc = new Calculator();
+    calc.perform("C");
+    test.done();
 };
 
 module.exports.clearOnSingle = function(test) {
-	var calc = new Calculator();
-	calc.enter(1);
-	calc.perform("C");
-	test.equals(0, calc.perform("+"));
-	test.done();
+    var calc = new Calculator();
+    calc.enter(1);
+    calc.perform("C");
+    test.equals(0, calc.perform("+"));
+    test.done();
 };
 
 module.exports.ensureSupportForWordClear = function(test) {
-	var calc = new Calculator();
-	calc.enter(1);
-	calc.perform("clear");
-	test.equals(0, calc.perform("+"));
-	test.done();
+    var calc = new Calculator();
+    calc.enter(1);
+    calc.perform("clear");
+    test.equals(0, calc.perform("+"));
+    test.done();
 };
 
 module.exports.ensureSupportForLowerCaseOperator = function(test) {
-	var calc = new Calculator();
-	calc.enter(1);
-	calc.perform("c");
-	test.equals(0, calc.perform("+"));
-	test.done();
+    var calc = new Calculator();
+    calc.enter(1);
+    calc.perform("c");
+    test.equals(0, calc.perform("+"));
+    test.done();
 };
 
 module.exports.clearOnList = function(test) {
-	var calc = new Calculator();
-	calc.enter(1);
-	calc.enter(1);
-	calc.enter(1);
-	calc.enter(1);
-	calc.enter(1);
-	calc.perform("C");
-	test.equals(0, calc.perform("+"));
-	test.done();
+    var calc = new Calculator();
+    calc.enter(1);
+    calc.enter(1);
+    calc.enter(1);
+    calc.enter(1);
+    calc.enter(1);
+    calc.perform("C");
+    test.equals(0, calc.perform("+"));
+    test.done();
 };
