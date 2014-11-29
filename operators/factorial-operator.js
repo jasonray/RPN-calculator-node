4ba3f19e4ba79d0a104189b0a23d7e17e441c1be
Jason Ray <jason.ray@agilex.com>
2014-11-29 10:13:43 -0500

    modify factorial to use monomial

--- operators/factorial-operator.js
+++ operators/factorial-operator.js
@@ -1,23 +1,27 @@
 /*jslint node: true */
 'use strict';
 
+var monomialOperator = require('./monomial-operator');
+
 module.exports = function(numbers) {
-    var n = numbers.pop();
+    return monomialOperator(numbers, factorial);
 
-    if (!isInteger(n)) {
-        throw new Error("Cannot perform factorial on non-integer");
+    function factorial(n) {
+        if (!isInteger(n)) {
+            throw new Error("Cannot perform factorial on non-integer");
+        }
+
+        var product = 1;
+
+        for (var i = n; i > 0; i--) {
+            product = product * i;
+        }
+
+        return product;
     }
-
-    var product = 1;
-
-    for (var i = n; i > 0; i--) {
-        product = product * i;
-    }
-
-    numbers.push(product);
-    return product;
 
     function isInteger(n) {
         return n % 1 === 0;
     }
+
 };
