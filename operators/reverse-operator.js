f5338e2d2b96c66eb92aac37904dc645d0d088ab
Jason Ray <jasonray@jayray.net>
2014-11-29 18:03:59 -0500

    fix reverse implementation

--- operators/reverse-operator.js
+++ operators/reverse-operator.js
@@ -4,13 +4,18 @@
 module.exports = function(numbers) {
 
     var tempNumbers = [];
+    var tempNumbers2 = [];
 
     while (!numbers.isEmpty()) {
         tempNumbers.push(numbers.pop());
     }
 
     for (var i = tempNumbers.length; i > 0; i--) {
-        numbers.push(tempNumbers[i - 1]);
+        tempNumbers2.push(tempNumbers[i - 1]);
+    }
+
+    for (var i = tempNumbers2.length; i > 0; i--) {
+        numbers.push(tempNumbers2[i - 1]);
     }
 
     return numbers.peek();
