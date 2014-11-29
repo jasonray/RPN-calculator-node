48692a65af35ff00c65e99c375bfde8da5ca46eb
Jason Ray <jason.ray@agilex.com>
2014-11-29 10:16:58 -0500

    formatting

--- calculator-commandline.js
+++ calculator-commandline.js
@@ -4,20 +4,20 @@ var Calculator = require('./calculator');
 
 var calculator = new Calculator();
 console.log("result: " + processCommandlineArgs(calculator));
-console.log("post-stack: " , calculator.numbers.toString());
+console.log("post-stack: ", calculator.numbers.toString());
 
 function processCommandlineArgs(calculator) {
-	var result = 0;
-	process.argv.forEach(function(val, index, array) {
-		if (index === 0) {
-			//this is likely 'node'
-		} else if (index === 1) {
-			//this is likely 'calculator.js'
-		} else if (isNaN(val)) {
-			result = calculator.perform(val);
-		} else {
-			result = calculator.enter(parseFloat(val, 10));
-		}
-	});
-	return result;
+    var result = 0;
+    process.argv.forEach(function(val, index, array) {
+        if (index === 0) {
+            //this is likely 'node'
+        } else if (index === 1) {
+            //this is likely 'calculator.js'
+        } else if (isNaN(val)) {
+            result = calculator.perform(val);
+        } else {
+            result = calculator.enter(parseFloat(val, 10));
+        }
+    });
+    return result;
 }
