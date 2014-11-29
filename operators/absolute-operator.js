498fe2de57c02295e12c04ad75323a9e970a7dbe
Jason Ray <jason.ray@agilex.com>
2014-11-29 09:44:51 -0500

    formatting

--- operators/absolute-operator.js
+++ operators/absolute-operator.js
@@ -2,10 +2,10 @@
 'use strict';
 
 module.exports = function(numbers) {
-	var n = numbers.pop();
-	if (n < 0) {
-		n = n * -1;
-	}
-	numbers.push(n);
-	return n;
+    var n = numbers.pop();
+    if (n < 0) {
+        n = n * -1;
+    }
+    numbers.push(n);
+    return n;
 };
