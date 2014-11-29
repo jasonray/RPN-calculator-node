3450cb090ec7959a03ae475bb16485d85249851d
Jason Ray <jason.ray@agilex.com>
2014-11-29 10:16:50 -0500

    formatting

--- operator-registry.js
+++ operator-registry.js
@@ -2,25 +2,25 @@
 'use strict';
 
 function Registry() {
-	var self = this;
-	self.handlers = {};
+    var self = this;
+    self.handlers = {};
 }
 
 Registry.prototype.registerHandler = function(operatorCharacter, operatorMethod) {
-	var self = this;
-	self.handlers[operatorCharacter.toLowerCase()] = operatorMethod;
+    var self = this;
+    self.handlers[operatorCharacter.toLowerCase()] = operatorMethod;
 };
 
 Registry.prototype.getOperatorMethod = function(operatorCharacter) {
-	var self = this;
-	var handler = self.handlers[operatorCharacter.toLowerCase()];
-	if (typeof handler === 'function') {
-		return handler;
-	} else if (typeof handler === 'object') {
-		return handler.doOperation;
-	} else {
-		return null;
-	}
+    var self = this;
+    var handler = self.handlers[operatorCharacter.toLowerCase()];
+    if (typeof handler === 'function') {
+        return handler;
+    } else if (typeof handler === 'object') {
+        return handler.doOperation;
+    } else {
+        return null;
+    }
 };
 
 module.exports = Registry;
