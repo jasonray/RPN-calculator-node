9086524862d1fc91c97d011be7cf25d09f69b22b
Jason Ray <jason.ray@agilex.com>
2014-11-29 10:16:54 -0500

    formatting

--- calculator.js
+++ calculator.js
@@ -6,58 +6,58 @@ var Registry = require("./operator-registry");
 
 
 function Calculator() {
-	var self = this;
+    var self = this;
 
-	self.numbers = new RpnStack();
-	self.operatorRegistry = initializeRegistry();
+    self.numbers = new RpnStack();
+    self.operatorRegistry = initializeRegistry();
 
-	function initializeRegistry() {
-		var registry = new Registry();
-		registry.registerHandler("+", require("./operators/addition-operator"));
-		registry.registerHandler("*", require("./operators/multiplication-operator"));
-		registry.registerHandler("x", require("./operators/multiplication-operator"));
-		registry.registerHandler("/", require("./operators/division-operator"));
-		registry.registerHandler("-", require("./operators/subtraction-operator"));
-		registry.registerHandler("||", require("./operators/absolute-operator"));
-		registry.registerHandler("average", require("./operators/average-operator"));
-		registry.registerHandler("ave", require("./operators/average-operator"));
-		registry.registerHandler("c", require("./operators/clear-operator"));
-		registry.registerHandler("clear", require("./operators/clear-operator"));
-		registry.registerHandler("fib", require("./operators/fibonacci-operator"));
-		registry.registerHandler("!", require("./operators/factorial-operator"));
-		registry.registerHandler("sum", require("./operators/sum-operator"));
-		registry.registerHandler("reverse", require("./operators/reverse-operator"));
-		return registry;
-	}
+    function initializeRegistry() {
+        var registry = new Registry();
+        registry.registerHandler("+", require("./operators/addition-operator"));
+        registry.registerHandler("*", require("./operators/multiplication-operator"));
+        registry.registerHandler("x", require("./operators/multiplication-operator"));
+        registry.registerHandler("/", require("./operators/division-operator"));
+        registry.registerHandler("-", require("./operators/subtraction-operator"));
+        registry.registerHandler("||", require("./operators/absolute-operator"));
+        registry.registerHandler("average", require("./operators/average-operator"));
+        registry.registerHandler("ave", require("./operators/average-operator"));
+        registry.registerHandler("c", require("./operators/clear-operator"));
+        registry.registerHandler("clear", require("./operators/clear-operator"));
+        registry.registerHandler("fib", require("./operators/fibonacci-operator"));
+        registry.registerHandler("!", require("./operators/factorial-operator"));
+        registry.registerHandler("sum", require("./operators/sum-operator"));
+        registry.registerHandler("reverse", require("./operators/reverse-operator"));
+        return registry;
+    }
 
-	return self;
+    return self;
 }
 
 Calculator.prototype.enter = function(operand) {
-	var self = this;
+    var self = this;
 
-	function isNumber(n) {
-		return !isNaN(parseFloat(n)) && isFinite(n);
-	}
+    function isNumber(n) {
+        return !isNaN(parseFloat(n)) && isFinite(n);
+    }
 
-	if (!isNumber(operand)) {
-		throw new Error("Cannot enter non-numeric values");
-	}
+    if (!isNumber(operand)) {
+        throw new Error("Cannot enter non-numeric values");
+    }
 
-	self.numbers.push(operand);
+    self.numbers.push(operand);
 
-	return operand;
+    return operand;
 };
 
 Calculator.prototype.perform = function(operatorCharacter) {
-	var self = this;
-	var operator = self.operatorRegistry.getOperatorMethod(operatorCharacter);
+    var self = this;
+    var operator = self.operatorRegistry.getOperatorMethod(operatorCharacter);
 
-	if (!operator) {
-		throw new Error("Unknown operator '" + operatorCharacter + "'");
-	}
+    if (!operator) {
+        throw new Error("Unknown operator '" + operatorCharacter + "'");
+    }
 
-	return operator(self.numbers);
+    return operator(self.numbers);
 };
 
 module.exports = Calculator;
