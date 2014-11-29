2ff5eb03968581531de25ade91c1459ae789032d
Jason Ray <jason.ray@agilex.com>
2014-11-29 09:45:00 -0500

    formatting

--- operators/average-operator.js
+++ operators/average-operator.js
@@ -2,21 +2,21 @@
 'use strict';
 
 module.exports = function(numbers) {
-	var count = 0;
-	var sum = 0;
+    var count = 0;
+    var sum = 0;
 
-	while (!numbers.isEmpty()) {
-		count = count + 1;
-		sum = sum + numbers.pop();
-	}
+    while (!numbers.isEmpty()) {
+        count = count + 1;
+        sum = sum + numbers.pop();
+    }
 
-	var result;
-	if (count === 0) {
-		result = 0;
-	} else {
-		result = sum / count;
-	}
+    var result;
+    if (count === 0) {
+        result = 0;
+    } else {
+        result = sum / count;
+    }
 
-	numbers.push(result);
-	return result;
+    numbers.push(result);
+    return result;
 };
