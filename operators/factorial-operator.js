8a27f30f38877d9ff91158f857f4979e53004f06
Jason Ray <jason.ray@agilex.com>
2014-11-29 09:45:12 -0500

    formatting

--- operators/factorial-operator.js
+++ operators/factorial-operator.js
@@ -2,22 +2,22 @@
 'use strict';
 
 module.exports = function(numbers) {
-	var n = numbers.pop();
+    var n = numbers.pop();
 
-	if (!isInteger(n)) {
-		throw new Error("Cannot perform factorial on non-integer");
-	}
+    if (!isInteger(n)) {
+        throw new Error("Cannot perform factorial on non-integer");
+    }
 
-	var product = 1;
+    var product = 1;
 
-	for (var i = n; i > 0; i--) {
-		product = product * i;
-	}
+    for (var i = n; i > 0; i--) {
+        product = product * i;
+    }
 
-	numbers.push(product);
-	return product;
+    numbers.push(product);
+    return product;
 
-	function isInteger(n) {
-		return (n === parseInt(n, 10));
-	}
+    function isInteger(n) {
+        return (n === parseInt(n, 10));
+    }
 };
