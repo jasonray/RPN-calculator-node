edd5e7bfb5edf1bee47fadae7e476c05df11fcc5
Jason Ray <jason.ray@agilex.com>
2014-11-29 10:04:40 -0500

    modify divide to utilize binary operation

--- operators/division-operator.js
+++ operators/division-operator.js
@@ -1,13 +1,13 @@
 /*jslint node: true */
 'use strict';
 
+var binaryOperation = require('./binary-operator');
+
 module.exports = function(numbers) {
-    var rhs = numbers.pop();
-    var lhs = numbers.pop();
+    return binaryOperation(numbers, divide);
 
-    if (rhs === 0) throw new Error("division by zero");
-
-    var result = lhs / rhs;
-    numbers.push(result);
-    return result;
+    function divide(lhs, rhs) {
+        if (rhs === 0) throw new Error("division by zero");
+        return lhs / rhs;
+    }
 };
