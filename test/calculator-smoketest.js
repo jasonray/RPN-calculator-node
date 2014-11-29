fcb6a55e9e77bf3b291c7f4ca18bbb4843ad7209
Jason Ray <jason.ray@agilex.com>
2014-11-29 09:45:47 -0500

    formatting

--- test/calculator-smoketest.js
+++ test/calculator-smoketest.js
@@ -4,30 +4,30 @@
 var Calculator = require('../calculator');
 
 module.exports.emptyAdd = function(test) {
-	var calc = new Calculator();
-	var result = calc.perform("+");
-	test.equals(0, result);
-	test.done();
+    var calc = new Calculator();
+    var result = calc.perform("+");
+    test.equals(0, result);
+    test.done();
 };
 
 module.exports.enterNonNumericValue = function(test) {
-	var calc = new Calculator();
-	test.throws(
-		function() {
-			calc.enter('a');
-		},
-		Error
-	);
-	test.done();
+    var calc = new Calculator();
+    test.throws(
+        function() {
+            calc.enter('a');
+        },
+        Error
+    );
+    test.done();
 };
 
 module.exports.handleUnknownOperatorCharacter = function(test) {
-	var calc = new Calculator();
-	test.throws(
-		function() {
-			calc.perform("unknown");
-		},
-		Error
-	);
-	test.done();
+    var calc = new Calculator();
+    test.throws(
+        function() {
+            calc.perform("unknown");
+        },
+        Error
+    );
+    test.done();
 };
