794977ecf08bd89b46a8d99860cd5bf980082d97
jason.ray <jasonray@jayray.net>
2016-12-04 11:28:12 -0500

    create sample test

--- mocha-test/sample-mocha-test.js
+++ mocha-test/sample-mocha-test.js
@@ -1,8 +1,12 @@
+/*jslint node: true */
+'use strict';
+
+var Calculator = require('../calculator');
 var assert = require('assert');
-describe('Array', function () {
-    describe('#indexOf()', function () {
-        it('should return -1 when the value is not present', function () {
-            assert.equal(-1, [1, 2, 3].indexOf(4));
-        });
+
+describe('operator name here', function () {
+    it('description here', function () {
+        var calc = new Calculator();
+        // do stuff
     });
 });
