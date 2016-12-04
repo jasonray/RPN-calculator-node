8759b1f10d1eec3df2173e4793281bad69a71943
jason.ray <jasonray@jayray.net>
2016-12-04 18:11:15 -0500

    setup factorial test

--- mocha-test/factorial-test.js
+++ mocha-test/factorial-test.js
@@ -2,21 +2,55 @@
 'use strict';
 
 var Calculator = require('../calculator');
+var assert = require('assert');
 
-module.exports.factorialReturnsProduct = function(test) {
+describe('factorial', function () {
+    it('factorial works on single number and returns product', function () {
+        var calc = new Calculator();
+        assert.fail(null, null, 'test not implemented');
+    });
+    it('factorial on nothing returns 1', function () {
+        var calc = new Calculator();
+        assert.fail(null, null, 'test not implemented');
+    });
+    it('ensure that factorial puts result on stack', function () {
+        var calc = new Calculator();
+        assert.fail(null, null, 'test not implemented');
+    });
+    it('execute factorial twice', function () {
+        var calc = new Calculator();
+        assert.fail(null, null, 'test not implemented');
+    });
+    it('factorial on non-integer throws error', function () {
+        var calc = new Calculator();
+        assert.fail(null, null, 'test not implemented');
+    });
+    it('factorial on negative integer throws error', function () {
+        var calc = new Calculator();
+        assert.fail(null, null, 'test not implemented');
+    });
+});
+
+
+/*jslint node: true */
+'use strict';
+
+var Calculator = require('../calculator');
+
+module.exports.factorialReturnsProduct = function (test) {
     var calc = new Calculator();
     calc.enter(4);
     test.equals(24, calc.perform("!"));
     test.done();
 };
 
-module.exports.factorialOnNothing = function(test) {
+module.exports.factorialOnNothing = function (test) {
     var calc = new Calculator();
     test.equals(1, calc.perform("!"));
     test.done();
 };
 
-module.exports.twoFactorials = function(test) {
+module.exports.twoFactorials = function (test) {
     var calc = new Calculator();
     calc.enter(3);
     calc.perform("!");
@@ -24,11 +58,11 @@ module.exports.twoFactorials = function(test) {
     test.done();
 };
 
-module.exports.nonInteger = function(test) {
+module.exports.nonInteger = function (test) {
     var calc = new Calculator();
     calc.enter(3.1);
 
-    test.throws(function() {
+    test.throws(function () {
         calc.perform("!");
     }, Error);
 
