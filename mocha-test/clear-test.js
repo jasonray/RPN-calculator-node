ebf27c81938951cdba7ad9418fecd841656d16ec
jason.ray <jasonray@jayray.net>
2016-12-04 12:58:20 -0500

    add clear tests

--- mocha-test/clear-test.js
+++ mocha-test/clear-test.js
@@ -24,6 +24,22 @@ describe('clear', function () {
         calc.perform("C");
         assert.equal(calc.peek(), 0);
     });
+    it('clear on a list of items stack clears stack', function () {
+        var calc = new Calculator();
+        calc.enter(1);
+        calc.enter(1);
+        calc.enter(1);
+        calc.enter(1);
+        calc.enter(1);
+        calc.enter(1);
+        calc.enter(1);
+        calc.enter(1);
+        calc.enter(1);
+        calc.enter(1);
+        calc.enter(1);
+        calc.perform("C");
+        assert.equal(calc.peek(), 0);
+    });
     it('ensure support for "C"', function () {
         var calc = new Calculator();
         calc.enter(1);
@@ -43,31 +59,3 @@ describe('clear', function () {
         assert.equal(calc.peek(), 0);
     });
 });
-
-module.exports.ensureSupportForWordClear = function (test) {
-    var calc = new Calculator();
-    calc.enter(1);
-    calc.perform("clear");
-    test.equals(0, calc.perform("+"));
-    test.done();
-};
-
-module.exports.ensureSupportForLowerCaseOperator = function (test) {
-    var calc = new Calculator();
-    calc.enter(1);
-    calc.perform("c");
-    test.equals(0, calc.perform("+"));
-    test.done();
-};
-
-module.exports.clearOnList = function (test) {
-    var calc = new Calculator();
-    calc.enter(1);
-    calc.enter(1);
-    calc.enter(1);
-    calc.enter(1);
-    calc.enter(1);
-    calc.perform("C");
-    test.equals(0, calc.perform("+"));
-    test.done();
-};
