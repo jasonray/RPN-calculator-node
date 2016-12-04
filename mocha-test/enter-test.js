d362d56ba97708604cfed5ed5def60d3c7ccc03a
jason.ray <jasonray@jayray.net>
2016-12-04 17:44:12 -0500

    update calc enter tests

--- mocha-test/enter-test.js
+++ mocha-test/enter-test.js
@@ -2,6 +2,30 @@
 'use strict';
 
 var Calculator = require('../calculator');
+var assert = require('assert');
+
+describe('calculator', function () {
+    describe('Enter', function () {
+        it('enter with string returns value', function () {
+            var calc = new Calculator();
+            assert.equal(1, calc.enter("1"))
+        });
+        it('enter with number returns value', function () {
+            var calc = new Calculator();
+            assert.equal(1, calc.enter(1))
+        });
+        it('enter with non integer number returns value', function () {
+            var calc = new Calculator();
+            assert.equal(1.1, calc.enter(1.1))
+        });
+    });
+});
+
+
+/*jslint node: true */
+'use strict';
+
+var Calculator = require('../calculator');
 
 module.exports.enterStringReturnsValue = function (test) {
     var calc = new Calculator();
