9dcd1762665f628be37a6c562015c25db6a45bb8
jason.ray <jasonray@jayray.net>
2016-12-04 12:45:23 -0500

    add smoketest

--- mocha-test/calculator-smoketest.js
+++ mocha-test/calculator-smoketest.js
@@ -13,40 +13,15 @@ describe('calculator', function () {
             var calc = new Calculator();
             assert.equal(calc.perform("+"), 0);
         });
+        it('enter on non-numeric value returns error, but allows for more entry', function () {
+            var calc = new Calculator();
+            assert.throws(
+                function () {
+                    calc.enter('a');
+                },
+                Error
+            );
+            assert.equal(calc.perform("+"), 0);
+        });
     });
 });
-
-
-/*jslint node: true */
-'use strict';
-
-var Calculator = require('../calculator');
-
-module.exports.emptyAdd = function (test) {
-    var calc = new Calculator();
-    var result = calc.perform("+");
-    test.equals(0, result);
-    test.done();
-};
-
-module.exports.enterNonNumericValue = function (test) {
-    var calc = new Calculator();
-    test.throws(
-        function () {
-            calc.enter('a');
-        },
-        Error
-    );
-    test.done();
-};
-
-module.exports.handleUnknownOperatorCharacter = function (test) {
-    var calc = new Calculator();
-    test.throws(
-        function () {
-            calc.perform("unknown");
-        },
-        Error
-    );
-    test.done();
-};
