cd8fe42274eb0becf6f77bb1543f7ba2ec5a3d48
jason.ray <jasonray@jayray.net>
2016-12-04 18:01:08 -0500

    update calc enter tests

--- mocha-test/enter-test.js
+++ mocha-test/enter-test.js
@@ -20,27 +20,3 @@ describe('calculator', function () {
         });
     });
 });
-
-
-/*jslint node: true */
-'use strict';
-
-var Calculator = require('../calculator');
-
-module.exports.enterStringReturnsValue = function (test) {
-    var calc = new Calculator();
-    test.equals(1, calc.enter("1"));
-    test.done();
-};
-
-module.exports.enterNumberReturnsValue = function (test) {
-    var calc = new Calculator();
-    test.equals(1, calc.enter(1));
-    test.done();
-};
-
-module.exports.enterNonInteger = function (test) {
-    var calc = new Calculator();
-    test.equals(1.5, calc.enter("1.5"));
-    test.done();
-};
