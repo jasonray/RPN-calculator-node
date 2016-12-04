261cbe259b94cc1efd6483578249eaa23dd2d3b4
jason.ray <jasonray@jayray.net>
2016-12-04 12:55:59 -0500

    cleanup

--- mocha-test/clear-test.js
+++ mocha-test/clear-test.js
@@ -26,27 +26,6 @@ describe('clear', function () {
     });
 });
 
-
-
-/*jslint node: true */
-'use strict';
-
-var Calculator = require('../calculator');
-
-module.exports.clearEmpty = function(test) {
-    var calc = new Calculator();
-    calc.perform("C");
-    test.done();
-};
-
-module.exports.clearOnSingle = function(test) {
-    var calc = new Calculator();
-    calc.enter(1);
-    calc.perform("C");
-    test.equals(0, calc.perform("+"));
-    test.done();
-};
-
 module.exports.ensureSupportForWordClear = function(test) {
     var calc = new Calculator();
     calc.enter(1);
