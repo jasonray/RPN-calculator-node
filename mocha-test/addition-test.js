035f388bae5c99bcb72e516b281515a8830dfa87
jason.ray <jasonray@jayray.net>
2016-12-04 12:06:50 -0500

    cleanup

--- mocha-test/addition-test.js
+++ mocha-test/addition-test.js
@@ -7,26 +7,34 @@ var assert = require('assert');
 describe('addition', function () {
     it('adding no numbers returns 0', function () {
         var calc = new Calculator();
-        assert.equal(0,calc.perform("+"))
+        assert.equal(0, calc.perform("+"))
     });
     it('adding two numbers returns the sum', function () {
         var calc = new Calculator();
         calc.enter(30);
         calc.enter(4);
-        assert.equal(34,calc.perform("+"))
+        assert.equal(34, calc.perform("+"))
     });
     it('with three numbers on the stack, adding add the top two and puts onto stack', function () {
         var calc = new Calculator();
         calc.enter(1);
         calc.enter(2);
         calc.enter(3);
-        assert.equal(5,calc.perform("+"))
-        assert.equal(5,calc.peek());
+        assert.equal(5, calc.perform("+"));
+        assert.equal(5, calc.peek());
+    });
+    it('with three numbers on the stack, adding twice results in the sum of those three numbers', function () {
+        var calc = new Calculator();
+        calc.enter(1);
+        calc.enter(2);
+        calc.enter(3);
+        calc.perform("+");
+        assert.equal(6, calc.perform("+"));
     });
 });
 
 
-module.exports.addThreeNumbers = function(test) {
+module.exports.addThreeNumbers = function (test) {
     var calc = new Calculator();
     calc.enter(1);
     calc.enter(2);
@@ -37,7 +45,7 @@ module.exports.addThreeNumbers = function(test) {
     test.done();
 };
 
-module.exports.addNegativeNumbers = function(test) {
+module.exports.addNegativeNumbers = function (test) {
     var calc = new Calculator();
     calc.enter(-1);
     calc.enter(-2);
@@ -46,7 +54,7 @@ module.exports.addNegativeNumbers = function(test) {
     test.done();
 };
 
-module.exports.addDecimals = function(test) {
+module.exports.addDecimals = function (test) {
     var calc = new Calculator();
     calc.enter(1.5);
     calc.enter(2.3);
