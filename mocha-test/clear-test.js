c85f96fb6d8449410e2fb2a14f8ea7e3589f62f7
jason.ray <jasonray@jayray.net>
2016-12-04 12:55:42 -0500

    add clear tests

--- mocha-test/clear-test.js
+++ mocha-test/clear-test.js
@@ -5,9 +5,24 @@ var Calculator = require('../calculator');
 var assert = require('assert');
 
 describe('clear', function () {
-    it('clear on a ', function () {
+    it('clear on a empty stack does not throw error', function () {
         var calc = new Calculator();
-        // do stuff
+        calc.perform("C");
+    });
+    it('clear on a single item stack clear stack', function () {
+        var calc = new Calculator();
+        calc.enter(1);
+        assert.equal(calc.peek(), 1);
+        calc.perform("C");
+        assert.equal(calc.peek(), 0);
+    });
+    it('clear on a two item stack clear stack', function () {
+        var calc = new Calculator();
+        calc.enter(1);
+        calc.enter(2);
+        assert.equal(calc.peek(), 2);
+        calc.perform("C");
+        assert.equal(calc.peek(), 0);
     });
 });
 
