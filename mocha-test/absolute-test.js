d06ee7ae8b388cba44810e23def5a8c7f9fa8222
jason.ray <jasonray@jayray.net>
2016-12-04 11:25:27 -0500

    move absolute test to mocha

--- mocha-test/absolute-test.js
+++ mocha-test/absolute-test.js
@@ -2,42 +2,45 @@
 'use strict';
 
 var Calculator = require('../calculator');
+var assert = require('assert');
 
-module.exports.empty = function(test) {
-    var calc = new Calculator();
-    test.equals(0, calc.perform("||"));
-    test.done();
-};
+describe('absolute', function () {
+    it('|| should be recognized by calculator', function () {
+        var calc = new Calculator();
+        assert.equal(0, calc.perform("||"));
+    });
+});
 
-module.exports.zero = function(test) {
+
+module.exports.zero = function (test) {
     var calc = new Calculator();
     calc.enter(0);
     test.equals(0, calc.perform("||"));
     test.done();
 };
 
-module.exports.one = function(test) {
+module.exports.one = function (test) {
     var calc = new Calculator();
     calc.enter(1);
     test.equals(1, calc.perform("||"));
     test.done();
 };
 
-module.exports.negOne = function(test) {
+module.exports.negOne = function (test) {
     var calc = new Calculator();
     calc.enter(-1);
     test.equals(1, calc.perform("||"));
     test.done();
 };
 
-module.exports.negTen = function(test) {
+module.exports.negTen = function (test) {
     var calc = new Calculator();
     calc.enter(-10);
     test.equals(10, calc.perform("||"));
     test.done();
 };
 
-module.exports.ensureResultIsOnStack = function(test) {
+module.exports.ensureResultIsOnStack = function (test) {
     var calc = new Calculator();
     calc.enter(1);
     calc.enter(-5);
@@ -46,7 +49,7 @@ module.exports.ensureResultIsOnStack = function(test) {
     test.done();
 };
 
-module.exports.decimal = function(test) {
+module.exports.decimal = function (test) {
     var calc = new Calculator();
     calc.enter(-1.5);
     test.equals(1.5, calc.perform("||"));
