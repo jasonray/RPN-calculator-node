a090f2311cf1b02fb679b9ce3d97995f70603f5e
jason.ray <jasonray@jayray.net>
2016-12-04 19:32:19 -0500

    add reverse test

--- mocha-test/reverse-test.js
+++ mocha-test/reverse-test.js
@@ -2,61 +2,57 @@
 'use strict';
 
 var Calculator = require('../calculator');
+var assert = require('assert');
 
-module.exports.reverseNoNumbers = function(test) {
-    var calc = new Calculator();
-    var result = calc.perform("reverse");
-    test.equals(0, result);
-    test.equals(0, calc.numbers.pop());
-    test.done();
-};
-
-module.exports.reverseOneNumber = function(test) {
-    var calc = new Calculator();
-    calc.enter(1);
-    var result = calc.perform("reverse");
-    test.equals(1, result);
-    test.equals(1, calc.numbers.pop());
-    test.equals(0, calc.numbers.pop());
-    test.done();
-};
-
-module.exports.reverseTwoNumbers = function(test) {
-    var calc = new Calculator();
-    calc.enter(1);
-    calc.enter(2);
-    var result = calc.perform("reverse");
-    test.equals(1, result);
-    test.equals(1, calc.numbers.pop());
-    test.equals(2, calc.numbers.pop());
-    test.equals(0, calc.numbers.pop());
-    test.done();
-};
-
-module.exports.reverseMultipleNumbers = function(test) {
-    var calc = new Calculator();
-    calc.enter(1);
-    calc.enter(2);
-    calc.enter(3);
-    calc.enter(4);
-    calc.enter(5);
-    calc.enter(6);
-    calc.enter(7);
-    calc.enter(8);
-    calc.enter(9);
-    calc.enter(10);
-    var result = calc.perform("reverse");
-    test.equals(1, result);
-    test.equals(1, calc.numbers.pop());
-    test.equals(2, calc.numbers.pop());
-    test.equals(3, calc.numbers.pop());
-    test.equals(4, calc.numbers.pop());
-    test.equals(5, calc.numbers.pop());
-    test.equals(6, calc.numbers.pop());
-    test.equals(7, calc.numbers.pop());
-    test.equals(8, calc.numbers.pop());
-    test.equals(9, calc.numbers.pop());
-    test.equals(10, calc.numbers.pop());
-    test.equals(0, calc.numbers.pop());
-    test.done();
-};
+describe('reverse', function () {
+    it('reverseNoNumbers', function () {
+        var calc = new Calculator();
+        var result = calc.perform("reverse");
+        assert.equal(0, result);
+        assert.equal(0, calc.numbers.pop());
+    });
+    it('reverseOneNumber', function () {
+        var calc = new Calculator();
+        calc.enter(1);
+        var result = calc.perform("reverse");
+        assert.equal(1, result);
+        assert.equal(1, calc.numbers.pop());
+        assert.equal(0, calc.numbers.pop());
+    });
+    it('reverseTwoNumbers', function () {
+        var calc = new Calculator();
+        calc.enter(1);
+        calc.enter(2);
+        var result = calc.perform("reverse");
+        assert.equal(1, result);
+        assert.equal(1, calc.numbers.pop());
+        assert.equal(2, calc.numbers.pop());
+        assert.equal(0, calc.numbers.pop());
+    });
+    it('reverseMultipleNumbers', function () {
+        var calc = new Calculator();
+        calc.enter(1);
+        calc.enter(2);
+        calc.enter(3);
+        calc.enter(4);
+        calc.enter(5);
+        calc.enter(6);
+        calc.enter(7);
+        calc.enter(8);
+        calc.enter(9);
+        calc.enter(10);
+        var result = calc.perform("reverse");
+        assert.equal(1, result);
+        assert.equal(1, calc.numbers.pop());
+        assert.equal(2, calc.numbers.pop());
+        assert.equal(3, calc.numbers.pop());
+        assert.equal(4, calc.numbers.pop());
+        assert.equal(5, calc.numbers.pop());
+        assert.equal(6, calc.numbers.pop());
+        assert.equal(7, calc.numbers.pop());
+        assert.equal(8, calc.numbers.pop());
+        assert.equal(9, calc.numbers.pop());
+        assert.equal(10, calc.numbers.pop());
+        assert.equal(0, calc.numbers.pop());
+    });
+});
