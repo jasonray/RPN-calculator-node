c6db13ee719f1e12dc416c4c195b5af460b820fb
jason.ray <jasonray@jayray.net>
2016-12-04 19:50:09 -0500

    add sum tests

--- mocha-test/sum-test.js
+++ mocha-test/sum-test.js
@@ -2,52 +2,44 @@
 'use strict';
 
 var Calculator = require('../calculator');
+var assert = require('assert');
 
-module.exports.sumNoNumbers = function(test) {
-    var calc = new Calculator();
-    test.equals(0, calc.perform("sum"));
-    test.done();
-};
-
-module.exports.sumOneNumber = function(test) {
-    var calc = new Calculator();
-    calc.enter(5);
-    test.equals(5, calc.perform("sum"));
-    test.done();
-};
-
-module.exports.sumTwoNumbers = function(test) {
-    var calc = new Calculator();
-    calc.enter(5);
-    calc.enter(3);
-    test.equals(8, calc.perform("sum"));
-    test.done();
-};
-
-module.exports.sumThreeNumbers = function(test) {
-    var calc = new Calculator();
-    calc.enter(5);
-    calc.enter(3);
-    calc.enter(1);
-    test.equals(9, calc.perform("sum"));
-    test.done();
-};
-
-module.exports.subtractThreeNumbers = function(test) {
-    var calc = new Calculator();
-    calc.enter(11);
-    calc.enter(5);
-    calc.enter(3);
-    calc.enter(1);
-    test.equals(20, calc.perform("sum"));
-    test.done();
-};
-
-module.exports.sumNonIntegers = function(test) {
-    var calc = new Calculator();
-    calc.enter(1.5);
-    calc.enter(3.5);
-    calc.enter(2.5);
-    test.equals(7.5, calc.perform("sum"));
-    test.done();
-};
+describe('sum', function () {
+    it('sumNoNumbers', function () {
+        var calc = new Calculator();
+        assert.equal(0, calc.perform("sum"));
+    });
+    it('sumOneNumber', function () {
+        var calc = new Calculator();
+        calc.enter(5);
+        assert.equal(5, calc.perform("sum"));
+    });
+    it('sumTwoNumbers', function () {
+        var calc = new Calculator();
+        calc.enter(5);
+        calc.enter(3);
+        assert.equal(8, calc.perform("sum"));
+    });
+    it('sumThreeNumbers', function () {
+        var calc = new Calculator();
+        calc.enter(5);
+        calc.enter(3);
+        calc.enter(1);
+        assert.equal(9, calc.perform("sum"));
+    });
+    it('subtractThreeNumbers', function () {
+        var calc = new Calculator();
+        calc.enter(11);
+        calc.enter(5);
+        calc.enter(3);
+        calc.enter(1);
+        assert.equal(20, calc.perform("sum"));
+    });
+    it('sumNonIntegers', function () {
+        var calc = new Calculator();
+        calc.enter(1.5);
+        calc.enter(3.5);
+        calc.enter(2.5);
+        assert.equal(7.5, calc.perform("sum"));
+    });
+});
