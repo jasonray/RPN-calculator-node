87d0044334f9d31d5cadc4d915c82c6bfd0027be
jason.ray <jasonray@jayray.net>
2016-12-04 19:22:10 -0500

    add multiply tests

--- mocha-test/multiplication-test.js
+++ mocha-test/multiplication-test.js
@@ -2,83 +2,69 @@
 'use strict';
 
 var Calculator = require('../calculator');
+var assert = require('assert');
 
-module.exports.multiplyNoNumbers = function(test) {
-    var calc = new Calculator();
-    var result = calc.perform("*");
-    test.equals(0, result);
-    test.done();
-};
-
-module.exports.multiplyTwoNumbersReturnsSum = function(test) {
-    var calc = new Calculator();
-    calc.enter(5);
-    calc.enter(4);
-    var result = calc.perform("*");
-    test.equals(20, result);
-    test.done();
-};
-
-module.exports.multiplyOneNumber = function(test) {
-    var calc = new Calculator();
-    calc.enter(4);
-    var result = calc.perform("*");
-    test.equals(0, result);
-    test.done();
-};
-
-module.exports.multiplyThreeNumbers = function(test) {
-    var calc = new Calculator();
-    calc.enter(4);
-    calc.enter(2);
-    calc.enter(3);
-    calc.perform("*");
-    var result = calc.perform("*");
-    test.equals(24, result);
-    test.done();
-};
-
-module.exports.multiplyNegativeNumbers = function(test) {
-    var calc = new Calculator();
-    calc.enter(-3);
-    calc.enter(-2);
-    var result = calc.perform("*");
-    test.equals(6, result);
-    test.done();
-};
-
-module.exports.multiplyWithx = function(test) {
-    var calc = new Calculator();
-    calc.enter(5);
-    calc.enter(4);
-    var result = calc.perform("x");
-    test.equals(20, result);
-    test.done();
-};
-
-module.exports.multiplyWithX = function(test) {
-    var calc = new Calculator();
-    calc.enter(5);
-    calc.enter(4);
-    var result = calc.perform("X");
-    test.equals(20, result);
-    test.done();
-};
-
-module.exports.multiplyWithNonInteger = function(test) {
-    var calc = new Calculator();
-    calc.enter(1.5);
-    calc.enter(4);
-    var result = calc.perform("X");
-    test.equals(6, result);
-    test.done();
-};
-
-module.exports.multiplyResultNonInteger = function(test) {
-    var calc = new Calculator();
-    calc.enter(1.5);
-    calc.enter(3);
-    var result = calc.perform("X");
-    test.equals(4.5, result);
-    test.done();
-};
+describe('multiplication', function () {
+    it('multiply empty stack is 0', function () {
+        var calc = new Calculator();
+        var result = calc.perform("*");
+        assert.equal(result, 0);
+    });
+    it('multiply two numbers returns product', function () {
+        var calc = new Calculator();
+        calc.enter(5);
+        calc.enter(4);
+        var result = calc.perform("*");
+        assert.equal(result, 20);
+    });
+    it('multiply one number returns 0', function () {
+        var calc = new Calculator();
+        calc.enter(4);
+        var result = calc.perform("*");
+        assert.equal(result, 0);
+    });
+    it('multiply three numbers', function () {
+        var calc = new Calculator();
+        calc.enter(4);
+        calc.enter(2);
+        calc.enter(3);
+        calc.perform("*");
+        var result = calc.perform("*");
+        assert.equal(result, 24);
+    });
+    it('multiply negative numbers', function () {
+        var calc = new Calculator();
+        calc.enter(-3);
+        calc.enter(-2);
+        var result = calc.perform("*");
+        assert.equal(result, 6);
+    });
+    it('use "x" operator', function () {
+        var calc = new Calculator();
+        calc.enter(5);
+        calc.enter(4);
+        var result = calc.perform("x");
+        assert.equal(result, 20);
+    });
+    it('use "X" operator', function () {
+        var calc = new Calculator();
+        calc.enter(5);
+        calc.enter(4);
+        var result = calc.perform("X");
+        assert.equal(result, 20);
+    });
+    it('multiply non integer numbers', function () {
+        var calc = new Calculator();
+        calc.enter(1.5);
+        calc.enter(4);
+        var result = calc.perform("X");
+        assert.equal(result, 6);
+    });
+    it('multiply with non integer result', function () {
+        var calc = new Calculator();
+        calc.enter(1.5);
+        calc.enter(3);
+        var result = calc.perform("X");
+        assert.equal(result, 4.5);
+    });
+});
