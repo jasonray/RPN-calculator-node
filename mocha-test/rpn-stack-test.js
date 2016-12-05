df87452979f134bc0d307c80eda48e761687d013
jason.ray <jasonray@jayray.net>
2016-12-04 19:39:09 -0500

    add rpn stack tests

--- mocha-test/rpn-stack-test.js
+++ mocha-test/rpn-stack-test.js
@@ -1,57 +1,51 @@
 /*jslint node: true */
 'use strict';
 
+/*jslint node: true */
+'use strict';
+
+var Calculator = require('../calculator');
 var RpnStack = require('../rpn-stack');
+var assert = require('assert');
 
-module.exports.popEmptyStack = function(test) {
-    var stack = new RpnStack();
-    var result = stack.pop();
-    test.equals(0, result);
-    test.done();
-};
-
-module.exports.peekEmptyStack = function(test) {
-    var stack = new RpnStack();
-    test.equals(0, stack.peek());
-    test.done();
-};
-
-module.exports.peekFromPush = function(test) {
-    var stack = new RpnStack();
-    stack.push(2);
-    test.equals(2, stack.peek());
-    test.done();
-};
-
-module.exports.popFronmPush = function(test) {
-    var stack = new RpnStack();
-    stack.push(2);
-    stack.push(5);
-    test.equals(5, stack.pop());
-    test.equals(2, stack.pop());
-    test.done();
-};
-
-module.exports.multiplePeeks = function(test) {
-    var stack = new RpnStack();
-    stack.push(2);
-    stack.push(5);
-    test.equals(5, stack.peek());
-    test.equals(5, stack.peek());
-    test.done();
-};
-
-module.exports.emptyStackWithPopsNextReturns0 = function(test) {
-    var stack = new RpnStack();
-    stack.push(2);
-    test.equals(2, stack.pop());
-    test.equals(0, stack.peek());
-    test.done();
-};
-
-module.exports.pushPop = function(test) {
-    var stack = new RpnStack();
-    stack.push(1);
-    test.equals(1, stack.pop());
-    test.done();
-}
+describe('rpn stack', function () {
+    it('popEmptyStack', function () {
+        var stack = new RpnStack();
+        var result = stack.pop();
+        assert.equal(0, result);
+    });
+    it('peekEmptyStack', function () {
+        var stack = new RpnStack();
+        assert.equal(0, stack.peek());
+    });
+    it('peekFromPush', function () {
+        var stack = new RpnStack();
+        stack.push(2);
+        assert.equal(2, stack.peek());
+    });
+    it('popFronmPush', function () {
+        var stack = new RpnStack();
+        stack.push(2);
+        stack.push(5);
+        assert.equal(5, stack.pop());
+        assert.equal(2, stack.pop());
+    });
+    it('multiplePeeks', function () {
+        var stack = new RpnStack();
+        stack.push(2);
+        stack.push(5);
+        assert.equal(5, stack.peek());
+        assert.equal(5, stack.peek());
+    });
+    it('emptyStackWithPopsNextReturns0', function () {
+        var stack = new RpnStack();
+        stack.push(2);
+        assert.equal(2, stack.pop());
+        assert.equal(0, stack.peek());
+    });
+    it('pushPop', function () {
+        var stack = new RpnStack();
+        stack.push(1);
+        assert.equal(1, stack.pop());
+    });
+});
