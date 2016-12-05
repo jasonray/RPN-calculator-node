160097d3ec01b1aa957691bd1ba95d58aa52aea5
jason.ray <jasonray@jayray.net>
2016-12-04 20:00:08 -0500

    cleanup

--- mocha-test/reverse-test.js
+++ mocha-test/reverse-test.js
@@ -8,7 +8,7 @@ describe('reverse', function () {
     it('reverseNoNumbers', function () {
         var calc = new Calculator();
         var result = calc.perform("reverse");
-        assert.equal(  result,0);
+        assert.equal(result, 0);
         assert.equal(0, calc.numbers.pop());
     });
     it('reverseOneNumber', function () {
