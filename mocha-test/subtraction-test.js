fafa734acfc0edf8a6583b5e0b29947660bf60e1
jason.ray <jasonray@jayray.net>
2016-12-04 19:46:51 -0500

    add subtraction tests

--- mocha-test/subtraction-test.js
+++ mocha-test/subtraction-test.js
@@ -2,56 +2,48 @@
 'use strict';
 
 var Calculator = require('../calculator');
+var assert = require('assert');
 
-module.exports.subtractNoNumbers = function(test) {
-    var calc = new Calculator();
-    var result = calc.perform("-");
-    test.equals(0, result);
-    test.done();
-};
-
-module.exports.subtractOneNumber = function(test) {
-    var calc = new Calculator();
-    calc.enter(1);
-    var result = calc.perform("-");
-    test.equals(-1, result);
-    test.done();
-};
-
-module.exports.subtractOneNumberTwice = function(test) {
-    var calc = new Calculator();
-    calc.enter(1);
-    calc.perform("-");
-    var result = calc.perform("-");
-    test.equals(1, result);
-    test.done();
-};
-
-module.exports.subtractTwoNumbers = function(test) {
-    var calc = new Calculator();
-    calc.enter(10);
-    calc.enter(3);
-    var result = calc.perform("-");
-    test.equals(7, result);
-    test.done();
-};
-
-module.exports.subtractThreeNumbers = function(test) {
-    var calc = new Calculator();
-    calc.enter(21);
-    calc.enter(13);
-    calc.perform("-");
-    calc.enter(5);
-    var result = calc.perform("-");
-    test.equals(3, result);
-    test.done();
-};
-
-module.exports.subtractWithNonInteger = function(test) {
-    var calc = new Calculator();
-    calc.enter(1.5);
-    calc.enter(0.3);
-    var result = calc.perform("-");
-    test.equals(1.2, result);
-    test.done();
-};
+describe('subtraction', function () {
+    it('subtractNoNumbers', function () {
+        var calc = new Calculator();
+        var result = calc.perform("-");
+        assert.equal(0, result);
+    });
+    it('subtractOneNumber', function () {
+        var calc = new Calculator();
+        calc.enter(1);
+        var result = calc.perform("-");
+        assert.equal(-1, result);
+    });
+    it('subtractOneNumberTwice', function () {
+        var calc = new Calculator();
+        calc.enter(1);
+        calc.perform("-");
+        var result = calc.perform("-");
+        assert.equal(1, result);
+    });
+    it('subtractTwoNumbers', function () {
+        var calc = new Calculator();
+        calc.enter(10);
+        calc.enter(3);
+        var result = calc.perform("-");
+        assert.equal(7, result);
+    });
+    it('subtractThreeNumbers', function () {
+        var calc = new Calculator();
+        calc.enter(21);
+        calc.enter(13);
+        calc.perform("-");
+        calc.enter(5);
+        var result = calc.perform("-");
+        assert.equal(3, result);
+    });
+    it('subtractWithNonInteger', function () {
+        var calc = new Calculator();
+        calc.enter(1.5);
+        calc.enter(0.3);
+        var result = calc.perform("-");
+        assert.equal(1.2, result);
+    });
+});
