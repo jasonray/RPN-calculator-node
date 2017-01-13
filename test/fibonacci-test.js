851f1d63f3424d7837fe595c3a8c3b4651cc700c
Jason Ray <jasonray@jayray.net>
2017-01-12 19:17:53 -0500

    use should assertions

--- test/fibonacci-test.js
+++ test/fibonacci-test.js
@@ -7,7 +7,6 @@ var assert = require('assert');
 describe('fibonacci', function () {
     it('fib on empty is 0', function () {
         var calc = new Calculator();
-        assert.equal(calc.perform("fib"), 0);
         calc.perform("fib").should.equal(0);
     });
     it('fib on 1 is 1', function () {
