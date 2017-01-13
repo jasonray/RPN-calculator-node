c21921fae3d6978132ff2a4650be3384975b7e9a
Jason Ray <jasonray@jayray.net>
2017-01-12 19:15:50 -0500

    use should assertions

--- test/sum-test.js
+++ test/sum-test.js
@@ -7,25 +7,25 @@ var assert = require('assert');
 describe('sum', function () {
     it('sumNoNumbers', function () {
         var calc = new Calculator();
-        assert.equal(calc.perform("sum"), 0);
+        calc.perform("sum").should.equal(0);
     });
     it('sumOneNumber', function () {
         var calc = new Calculator();
         calc.enter(5);
-        assert.equal(calc.perform("sum"), 5);
+        calc.perform("sum").should.equal(5);
     });
     it('sumTwoNumbers', function () {
         var calc = new Calculator();
         calc.enter(5);
         calc.enter(3);
-        assert.equal(calc.perform("sum"), 8);
+        calc.perform("sum").should.equal(8);
     });
     it('sumThreeNumbers', function () {
         var calc = new Calculator();
         calc.enter(5);
         calc.enter(3);
         calc.enter(1);
-        assert.equal(calc.perform("sum"), 9);
+        calc.perform("sum").should.equal(9);
     });
     it('subtractThreeNumbers', function () {
         var calc = new Calculator();
@@ -33,13 +33,13 @@ describe('sum', function () {
         calc.enter(5);
         calc.enter(3);
         calc.enter(1);
-        assert.equal(calc.perform("sum"), 20);
+        calc.perform("sum").should.equal(20);
     });
     it('sumNonIntegers', function () {
         var calc = new Calculator();
         calc.enter(1.5);
         calc.enter(3.5);
         calc.enter(2.5);
-        assert.equal(calc.perform("sum"), 7.5);
+        calc.perform("sum").should.equal(7.5);
     });
 });
