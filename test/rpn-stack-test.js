ce68db1a6afc1d15beaad57c1dfe4d670408b4a5
Jason Ray <jasonray@jayray.net>
2017-01-12 19:13:23 -0500

    use should assertions

--- test/rpn-stack-test.js
+++ test/rpn-stack-test.js
@@ -11,40 +11,40 @@ var assert = require('assert');
 describe('rpn stack', function () {
     it('popEmptyStack', function () {
         var stack = new RpnStack();
-        assert.equal(stack.pop(), 0);
+        stack.pop().should.equal(0);
     });
     it('peekEmptyStack', function () {
         var stack = new RpnStack();
-        assert.equal(stack.peek(), 0);
+        stack.peek().should.equal(0);
     });
     it('peekFromPush', function () {
         var stack = new RpnStack();
         stack.push(2);
-        assert.equal(stack.peek(), 2);
+        stack.peek().should.equal(2);
     });
     it('popFronmPush', function () {
         var stack = new RpnStack();
         stack.push(2);
         stack.push(5);
-        assert.equal(stack.pop(), 5);
-        assert.equal(stack.pop(), 2);
+        stack.pop().should.equal(5);
+        stack.pop().should.equal(2);
     });
     it('multiplePeeks', function () {
         var stack = new RpnStack();
         stack.push(2);
         stack.push(5);
-        assert.equal(stack.peek(), 5);
-        assert.equal(stack.peek(), 5);
+        stack.peek().should.equal(5);
+        stack.peek().should.equal(5);
     });
     it('emptyStackWithPopsNextReturns0', function () {
         var stack = new RpnStack();
         stack.push(2);
-        assert.equal(stack.pop(), 2);
-        assert.equal(stack.peek(), 0);
+        stack.pop().should.equal(2);
+        stack.peek().should.equal(0);
     });
     it('pushPop', function () {
         var stack = new RpnStack();
         stack.push(1);
-        assert.equal(stack.pop(), 1);
+        stack.pop().should.equal(1);
     });
 });
