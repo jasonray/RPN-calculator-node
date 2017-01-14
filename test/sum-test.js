389e1986e1d858344306dc8591f3822039c2d084
Jason Ray <jasonray@jayray.net>
2017-01-13 21:02:39 -0500

    reduce scope on variable

--- test/sum-test.js
+++ test/sum-test.js
@@ -1,34 +1,34 @@
 /*jslint node: true */
 'use strict';
 
-var Calculator = require('../lib/calculator');
-var should = require('should');
+const Calculator = require('../lib/calculator');
+const should = require('should');
 
 describe('sum', function () {
     it('sumNoNumbers', function () {
-        var calc = new Calculator();
+        const calc = new Calculator();
         calc.perform("sum").should.equal(0);
     });
     it('sumOneNumber', function () {
-        var calc = new Calculator();
+        const calc = new Calculator();
         calc.enter(5);
         calc.perform("sum").should.equal(5);
     });
     it('sumTwoNumbers', function () {
-        var calc = new Calculator();
+        const calc = new Calculator();
         calc.enter(5);
         calc.enter(3);
         calc.perform("sum").should.equal(8);
     });
     it('sumThreeNumbers', function () {
-        var calc = new Calculator();
+        const calc = new Calculator();
         calc.enter(5);
         calc.enter(3);
         calc.enter(1);
         calc.perform("sum").should.equal(9);
     });
     it('subtractThreeNumbers', function () {
-        var calc = new Calculator();
+        const calc = new Calculator();
         calc.enter(11);
         calc.enter(5);
         calc.enter(3);
@@ -36,7 +36,7 @@ describe('sum', function () {
         calc.perform("sum").should.equal(20);
     });
     it('sumNonIntegers', function () {
-        var calc = new Calculator();
+        const calc = new Calculator();
         calc.enter(1.5);
         calc.enter(3.5);
         calc.enter(2.5);
