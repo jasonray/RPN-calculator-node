991f8636630c45e1811635c02f3f469c8e64d332
Jason Ray <jasonray@jayray.net>
2017-01-13 20:59:48 -0500

    reduce scope on variable

--- test/clear-test.js
+++ test/clear-test.js
@@ -1,23 +1,23 @@
 /*jslint node: true */
 'use strict';
 
-var Calculator = require('../lib/calculator');
-var should = require('should');
+const Calculator = require('../lib/calculator');
+const should = require('should');
 
 describe('clear', function () {
     it('clear on a empty stack does not throw error', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.perform("C");
     });
     it('clear on a single item stack clear stack', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(1);
         calc.peek().should.equal(1);
         calc.perform("C");
         calc.peek().should.equal(0);
     });
     it('clear on a two item stack clear stack', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(1);
         calc.enter(2);
         calc.peek().should.equal(2);
@@ -25,7 +25,7 @@ describe('clear', function () {
         calc.peek().should.equal(0);
     });
     it('clear on a list of items stack clears stack', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(1);
         calc.enter(1);
         calc.enter(1);
@@ -40,7 +40,7 @@ describe('clear', function () {
         calc.peek().should.equal(0);
     });
     it('ensure support for "C"', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(1);
         calc.peek().should.equal(1);
 
@@ -48,14 +48,14 @@ describe('clear', function () {
         calc.peek().should.equal(0);
     });
     it('ensure support for "c"', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(1);
         calc.peek().should.equal(1);
         calc.perform("c");
         calc.peek().should.equal(0);
     });
     it('ensure support for "clear"', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(1);
         calc.peek().should.equal(1);
         calc.perform("c");
