59e178e262499f66ba7560fb3a2604da16388a35
Jason Ray <jasonray@jayray.net>
2017-01-13 21:02:29 -0500

    reduce scope on variable

--- test/subtraction-test.js
+++ test/subtraction-test.js
@@ -1,33 +1,33 @@
 /*jslint node: true */
 'use strict';
 
-var Calculator = require('../lib/calculator');
-var should = require('should');
+const Calculator = require('../lib/calculator');
+const should = require('should');
 
 describe('subtraction', function () {
     it('subtractNoNumbers', function () {
-        var calc = new Calculator();
+        const calc = new Calculator();
         calc.perform("-").should.equal(0);
     });
     it('subtractOneNumber', function () {
-        var calc = new Calculator();
+        const calc = new Calculator();
         calc.enter(1);
         calc.perform("-").should.equal(-1);
     });
     it('subtractOneNumberTwice', function () {
-        var calc = new Calculator();
+        const calc = new Calculator();
         calc.enter(1);
         calc.perform("-");
         calc.perform("-").should.equal(1);
     });
     it('subtractTwoNumbers', function () {
-        var calc = new Calculator();
+        const calc = new Calculator();
         calc.enter(10);
         calc.enter(3);
         calc.perform("-").should.equal(7);
     });
     it('subtractThreeNumbers', function () {
-        var calc = new Calculator();
+        const calc = new Calculator();
         calc.enter(21);
         calc.enter(13);
         calc.perform("-");
@@ -35,7 +35,7 @@ describe('subtraction', function () {
         calc.perform("-").should.equal(3);
     });
     it('subtractWithNonInteger', function () {
-        var calc = new Calculator();
+        const calc = new Calculator();
         calc.enter(1.5);
         calc.enter(0.3);
         calc.perform("-").should.equal(1.2);
