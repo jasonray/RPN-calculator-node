de6d4e5c64b3e81793d4200bc4ffac53f36250f6
Jason Ray <jasonray@jayray.net>
2017-01-13 21:00:47 -0500

    reduce scope on variable

--- test/factorial-test.js
+++ test/factorial-test.js
@@ -1,35 +1,35 @@
 /*jslint node: true */
 'use strict';
 
-var Calculator = require('../lib/calculator');
-var assert = require('assert');
-var should = require('should');
+const Calculator = require('../lib/calculator');
+const assert = require('assert');
+const should = require('should');
 
 
 describe('factorial', function () {
     it('factorial works on single number and returns product', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(4);
         calc.perform("!").should.equal(24);
     });
     it('factorial on nothing returns 1', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.perform("!").should.equal(1);
     });
     it('ensure that factorial puts result on stack', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(3);
         calc.perform("!");
         calc.peek().should.equal(6);
     });
     it('execute factorial twice', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(3);
         calc.perform("!");
         calc.perform("!").should.equal(720);
     });
     it('factorial on non-integer throws error', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(1.1);
         assert.throws(
             function () {
@@ -39,7 +39,7 @@ describe('factorial', function () {
         );
     });
     it('factorial on negative integer throws error', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(-2);
         assert.throws(
             function () {
