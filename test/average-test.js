f55c0aea92325afa28c08b0ad64860cd06b60daa
Jason Ray <jasonray@jayray.net>
2017-01-13 20:59:00 -0500

    reduce scope on variable

--- test/average-test.js
+++ test/average-test.js
@@ -1,44 +1,44 @@
 /*jslint node: true */
 'use strict';
 
-var Calculator = require('../lib/calculator');
-var should = require('should');
+const Calculator = require('../lib/calculator');
+const should = require('should');
 
 describe('average', function () {
     it('average on empty stack is 0', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.perform('AVE').should.equal(0);
     });
     it('average of 0 is 0', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(0);
         calc.perform('AVE').should.equal(0);
     });
     it('average of x is x', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(5);
         calc.perform('AVE').should.equal(5);
     });
     it('average of x is x (with negative numbers)', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(-5);
         calc.perform('AVE').should.equal(-5);
     });
     it('average two numbers', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(1);
         calc.enter(3);
         calc.perform('AVE').should.equal(2);
     });
     it('average three numbers', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(1);
         calc.enter(3);
         calc.enter(5);
         calc.perform('AVE').should.equal(3);
     });
     it('average five numbers', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(1);
         calc.enter(2);
         calc.enter(3);
@@ -47,7 +47,7 @@ describe('average', function () {
         calc.perform('AVE').should.equal(3);
     });
     it('average numbers (with negative numbers)', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(10);
         calc.enter(-20);
         calc.enter(50);
@@ -55,13 +55,13 @@ describe('average', function () {
         calc.perform('AVE').should.equal(11);
     });
     it('average numbers (using "AVERAGE" operator)', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(1);
         calc.enter(3);
         calc.perform('AVERAGE').should.equal(2);
     });
     it('operator not case sensitive', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(1);
         calc.enter(3);
         calc.perform('ave').should.equal(2);
@@ -71,19 +71,19 @@ describe('average', function () {
         //then average with 2 would be 2
         //if empty average does put 0 onto stack
         //then average with 2 would be 1 (0,1)
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.perform("ave");
         calc.enter(2);
         calc.perform('AVE').should.equal(1);
     });
     it('average two numbers with non integer result', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(1);
         calc.enter(4);
         calc.perform('AVE').should.equal(2.5);
     });
     it('average two non-integer numbers with non integer result', function () {
-        var calc = new Calculator();
+        let calc = new Calculator();
         calc.enter(1.5);
         calc.enter(3.5);
         calc.perform('AVE').should.equal(2.5);
