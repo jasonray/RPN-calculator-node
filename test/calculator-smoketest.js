cf0104ce0eb1b813192a84706ded9cda60f9f500
Jason Ray <jasonray@jayray.net>
2017-01-13 20:59:26 -0500

    reduce scope on variable

--- test/calculator-smoketest.js
+++ test/calculator-smoketest.js
@@ -1,22 +1,21 @@
 /*jslint node: true */
 'use strict';
 
-var Calculator = require('../lib/calculator');
-var assert = require('assert');
-var should = require('should');
-
+const Calculator = require('../lib/calculator');
+const assert = require('assert');
+const should = require('should');
 
 describe('calculator', function () {
     describe('smoke test', function () {
         it('constructor', function () {
-            var calc = new Calculator();
+            let calc = new Calculator();
         });
         it('empty add', function () {
-            var calc = new Calculator();
+            let calc = new Calculator();
             calc.perform('+').should.equal(0);
         });
         it('enter on non-numeric value returns error, but allows for more entry', function () {
-            var calc = new Calculator();
+            let calc = new Calculator();
             assert.throws(
                 function () {
                     calc.enter('a');
