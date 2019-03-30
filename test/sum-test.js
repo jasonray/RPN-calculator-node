bbd2fb9d26800f46cb87e153533db7d418553fc1
Jason Ray <jasonray@jayray.net>
2019-03-29 21:47:48 -0400

    single vs double quote warning

--- test/sum-test.js
+++ test/sum-test.js
@@ -7,25 +7,25 @@ const should = require('should');
 describe('sum', function () {
     it('sumNoNumbers', function () {
         const calc = new Calculator();
-        calc.perform("sum").should.equal(0);
+        calc.perform('sum').should.equal(0);
     });
     it('sumOneNumber', function () {
         const calc = new Calculator();
         calc.enter(5);
-        calc.perform("sum").should.equal(5);
+        calc.perform('sum').should.equal(5);
     });
     it('sumTwoNumbers', function () {
         const calc = new Calculator();
         calc.enter(5);
         calc.enter(3);
-        calc.perform("sum").should.equal(8);
+        calc.perform('sum').should.equal(8);
     });
     it('sumThreeNumbers', function () {
         const calc = new Calculator();
         calc.enter(5);
         calc.enter(3);
         calc.enter(1);
-        calc.perform("sum").should.equal(9);
+        calc.perform('sum').should.equal(9);
     });
     it('subtractThreeNumbers', function () {
         const calc = new Calculator();
@@ -33,13 +33,13 @@ describe('sum', function () {
         calc.enter(5);
         calc.enter(3);
         calc.enter(1);
-        calc.perform("sum").should.equal(20);
+        calc.perform('sum').should.equal(20);
     });
     it('sumNonIntegers', function () {
         const calc = new Calculator();
         calc.enter(1.5);
         calc.enter(3.5);
         calc.enter(2.5);
-        calc.perform("sum").should.equal(7.5);
+        calc.perform('sum').should.equal(7.5);
     });
 });
