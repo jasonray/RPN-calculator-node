547874546c3d18c7a1de7e924736eaa9faf31261
Jason Ray <jasonray@jayray.net>
2019-03-29 21:48:47 -0400

    single vs double quote warning

--- test/subtraction-test.js
+++ test/subtraction-test.js
@@ -7,37 +7,37 @@ const should = require('should');
 describe('subtraction', function () {
     it('subtractNoNumbers', function () {
         const calc = new Calculator();
-        calc.perform("-").should.equal(0);
+        calc.perform('-').should.equal(0);
     });
     it('subtractOneNumber', function () {
         const calc = new Calculator();
         calc.enter(1);
-        calc.perform("-").should.equal(-1);
+        calc.perform('-').should.equal(-1);
     });
     it('subtractOneNumberTwice', function () {
         const calc = new Calculator();
         calc.enter(1);
-        calc.perform("-");
-        calc.perform("-").should.equal(1);
+        calc.perform('-');
+        calc.perform('-').should.equal(1);
     });
     it('subtractTwoNumbers', function () {
         const calc = new Calculator();
         calc.enter(10);
         calc.enter(3);
-        calc.perform("-").should.equal(7);
+        calc.perform('-').should.equal(7);
     });
     it('subtractThreeNumbers', function () {
         const calc = new Calculator();
         calc.enter(21);
         calc.enter(13);
-        calc.perform("-");
+        calc.perform('-');
         calc.enter(5);
-        calc.perform("-").should.equal(3);
+        calc.perform('-').should.equal(3);
     });
     it('subtractWithNonInteger', function () {
         const calc = new Calculator();
         calc.enter(1.5);
         calc.enter(0.3);
-        calc.perform("-").should.equal(1.2);
+        calc.perform('-').should.equal(1.2);
     });
 });
