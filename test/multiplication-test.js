1215d9e6a02875286e85f07efb5c51a269da85fc
Jason Ray <jasonray@jayray.net>
2019-03-29 21:58:26 -0400

    single vs double quote warning

--- test/multiplication-test.js
+++ test/multiplication-test.js
@@ -7,32 +7,32 @@ const should = require('should');
 describe('multiplication', function () {
     it('multiply empty stack is 0', function () {
         let calc = new Calculator();
-        calc.perform("*").should.equal(0);
+        calc.perform('*').should.equal(0);
     });
     it('multiply two numbers returns product', function () {
         let calc = new Calculator();
         calc.enter(5);
         calc.enter(4);
-        calc.perform("*").should.equal(20);
+        calc.perform('*').should.equal(20);
     });
     it('multiply one number returns 0', function () {
         let calc = new Calculator();
         calc.enter(4);
-        calc.perform("*").should.equal(0);
+        calc.perform('*').should.equal(0);
     });
     it('multiply three numbers', function () {
         let calc = new Calculator();
         calc.enter(4);
         calc.enter(2);
         calc.enter(3);
-        calc.perform("*");
-        calc.perform("*").should.equal(24);
+        calc.perform('*');
+        calc.perform('*').should.equal(24);
     });
     it('multiply negative numbers', function () {
         let calc = new Calculator();
         calc.enter(-3);
         calc.enter(-2);
-        calc.perform("*").should.equal(6);
+        calc.perform('*').should.equal(6);
     });
     it('use "x" operator', function () {
         let calc = new Calculator();
