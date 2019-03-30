2f597cc82ddcc37f396676788ce418ab96f29e78
Jason Ray <jasonray@jayray.net>
2019-03-29 22:23:33 -0400

    quote style

--- test/addition-test.js
+++ test/addition-test.js
@@ -15,20 +15,20 @@ function assertFloatsEqual(actual, expected) {
 describe('addition', function () {
     it('adding no numbers returns 0', function () {
         let calc = new Calculator();
-        calc.perform("+").should.equal(0);
+        calc.perform('+').should.equal(0);
     });
     it('adding two numbers returns the sum', function () {
         let calc = new Calculator();
         calc.enter(30);
         calc.enter(4);
-        calc.perform("+").should.equal(34);
+        calc.perform('+').should.equal(34);
     });
     it('with three numbers on the stack, adding add the top two and puts onto stack', function () {
         let calc = new Calculator();
         calc.enter(1);
         calc.enter(2);
         calc.enter(3);
-        calc.perform("+").should.equal(5);
+        calc.perform('+').should.equal(5);
         calc.peek().should.equal(5);
     });
     it('with three numbers on the stack, adding twice results in the sum of those three numbers', function () {
@@ -36,19 +36,19 @@ describe('addition', function () {
         calc.enter(1);
         calc.enter(2);
         calc.enter(3);
-        calc.perform("+");
-        calc.perform("+").should.equal(6);
+        calc.perform('+');
+        calc.perform('+').should.equal(6);
     });
     it('adding two numbers returns the sum (with negative numbers)', function () {
         let calc = new Calculator();
         calc.enter(-1);
         calc.enter(-2);
-        calc.perform("+").should.equal(-3);
+        calc.perform('+').should.equal(-3);
     });
     it('adding two numbers returns the sum (with decimal numbers)', function () {
         let calc = new Calculator();
         calc.enter(2.1);
         calc.enter(3.2);
-        assertFloatsEqual(calc.perform("+"), 5.3);
+        assertFloatsEqual(calc.perform('+'), 5.3);
     });
 });
