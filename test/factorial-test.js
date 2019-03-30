07ec30c9ed0743bca2a4f1906fc00f20f94d67ce
Jason Ray <jasonray@jayray.net>
2019-03-29 21:57:51 -0400

    single vs double quote warning

--- test/factorial-test.js
+++ test/factorial-test.js
@@ -9,11 +9,11 @@ describe('factorial', function () {
     it('factorial works on single number and returns product', function () {
         let calc = new Calculator();
         calc.enter(4);
-        calc.perform("!").should.equal(24);
+        calc.perform('!').should.equal(24);
     });
     it('factorial on nothing returns 1', function () {
         let calc = new Calculator();
-        calc.perform("!").should.equal(1);
+        calc.perform('!').should.equal(1);
     });
     it('ensure that factorial puts result on stack', function () {
         let calc = new Calculator();
@@ -24,8 +24,8 @@ describe('factorial', function () {
     it('execute factorial twice', function () {
         let calc = new Calculator();
         calc.enter(3);
-        calc.perform("!");
-        calc.perform("!").should.equal(720);
+        calc.perform('!');
+        calc.perform('!').should.equal(720);
     });
     it('factorial on non-integer throws error', function () {
         let calc = new Calculator();
