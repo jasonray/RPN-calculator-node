b594905cbda958e9deee8822f3d7021b21080a66
Jason Ray <jason.ray@ablevets.com>
2019-09-23 16:05:39 -0400

    initial pi test

---
+++ test/pi-test.js
@@ -0,0 +1,29 @@
+/*jslint node: true */
+'use strict';
+
+const Calculator = require('../lib/calculator');
+const assert = require('assert');
+const should = require('should');
+
+describe('pi', function () {
+    it('pi on empty is 0', function () {
+        let calc = new Calculator();
+        calc.perform('fib').should.equal(0);
+    });
+    // it('pi on 1 is 3', function () {
+    //     let calc = new Calculator();
+    //     calc.enter(3);
+    //     calc.perform('pi').should.equal(1);
+    // });
+
+    // it('fib on non integer is not defined', function () {
+    //     let calc = new Calculator();
+    //     calc.enter(1.5);
+    //     assert.throws(
+    //         function () {
+    //             calc.perform('fib');
+    //         },
+    //         Error
+    //     );
+    // });
+});
