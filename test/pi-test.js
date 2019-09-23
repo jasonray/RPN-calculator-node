caba193b0a3968173757486a61a8720ae94b5f44
Jason Ray <jason.ray@ablevets.com>
2019-09-23 17:36:43 -0400

    shift meaning of nth of pi

--- test/pi-test.js
+++ test/pi-test.js
@@ -6,42 +6,38 @@ const assert = require('assert');
 const should = require('should');
 
 describe('pi', function () {
-    it('pi on empty is 0', function () {
-        let calc = new Calculator();
-        calc.perform('pi').should.equal(0);
+    it('pi on 0', function () {
+        runPiTest(0,3);
     });
     it('pi on 1', function () {
-        runPiTest(1,3);
+        runPiTest(1,1);
     });
     it('pi on 2', function () {
-        runPiTest(2,1);
+        runPiTest(2,4);
     });
     it('pi on 3', function () {
-        runPiTest(3,4);
+        runPiTest(3,1);
     });
     it('pi on 4', function () {
-        runPiTest(4,1);
+        runPiTest(4,5);
     });
     it('pi on 5', function () {
-        runPiTest(5,5);
+        runPiTest(5,9);
     });
     it('pi on 6', function () {
-        runPiTest(6,9);
+        runPiTest(6,2);
     });
     it('pi on 7', function () {
-        runPiTest(7,2);
+        runPiTest(7,6);
     });
     it('pi on 8', function () {
-        runPiTest(8,6);
+        runPiTest(8,5);
     });
     it('pi on 9', function () {
-        runPiTest(9,5);
+        runPiTest(9,3);
     });
     it('pi on 10', function () {
-        runPiTest(10,3);
-    });
-    it('pi on 11', function () {
-        runPiTest(11,5);
+        runPiTest(10,5);
     });
 
     function runPiTest(nthNumber, expected) {
