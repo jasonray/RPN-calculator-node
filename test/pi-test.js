e190f1d56db2f216a8d97d2ca46f03c4314f0be7
Jason Ray <jason.ray@ablevets.com>
2019-09-23 17:17:06 -0400

    refactor test method

--- test/pi-test.js
+++ test/pi-test.js
@@ -5,16 +5,22 @@ const Calculator = require('../lib/calculator');
 const assert = require('assert');
 const should = require('should');
 
+31415926535
+
 describe('pi', function () {
     it('pi on empty is 0', function () {
         let calc = new Calculator();
         calc.perform('pi').should.equal(0);
     });
-    it('pi on 1 is 3', function () {
-        let calc = new Calculator();
-        calc.enter(1);
-        calc.perform('pi').should.equal(3);
+    it('pi on 1', function () {
+        runPiTest(1,3);
     });
+
+    function runPiTest(nthNumber, expected) {
+        let calc = new Calculator();
+        calc.enter(nthNumber);
+        calc.perform('pi').should.equal(expected);
+    }
 
     // it('fib on non integer is not defined', function () {
     //     let calc = new Calculator();
