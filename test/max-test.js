e5dbd9503c494795a13e265192a310dc04104d61
Jason Ray <jason.ray@ablevets.com>
2019-10-02 13:57:24 -0400

    validate max test

--- test/max-test.js
+++ test/max-test.js
@@ -5,7 +5,7 @@ const Calculator = require('../lib/calculator');
 const should = require('should');
 
 describe('max', function () {
-    it.skip('max with no numbers is 0', function () {
+    it('max with no numbers is 0', function () {
         const calc = new Calculator();
         calc.perform('max').should.equal(0);
     });
