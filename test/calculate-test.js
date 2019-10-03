203485b3938f8e19793bf5a84e654b90cdddb99d
Jason Ray <jasonray@jayray.net>
2019-10-03 19:41:03 -0400

    additional calculate test

--- test/calculate-test.js
+++ test/calculate-test.js
@@ -27,4 +27,8 @@ const should = require('should');
             let calc = new Calculator();
             calc.calculate('3 1 2 +').should.equal(3);
         });
+        it.skip('multiple operations return value', function () {
+            let calc = new Calculator();
+            calc.calculate('3 1 2 + +').should.equal(6);
+        });
     });
