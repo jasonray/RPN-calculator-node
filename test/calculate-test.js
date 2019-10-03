c469f0e17f4917691c24065f5d132732877309f4
Jason Ray <jasonray@jayray.net>
2019-10-03 19:31:53 -0400

    initial calculator calculate test

--- test/calculate-test.js
+++ test/calculate-test.js
@@ -6,7 +6,7 @@ const assert = require('assert');
 const should = require('should');
 
     describe('calculate', function () {
-        it.skkp('binary operation returns answer', function () {
+        it.skip('binary operation returns answer', function () {
             let calc = new Calculator();
             calc.calculate('1 2 +').should.equal(3);
         });
