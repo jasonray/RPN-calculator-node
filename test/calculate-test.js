b3b0621c2c9db082073b57f05e7023cc2dbaa1c2
Jason Ray <jasonray@jayray.net>
2019-10-03 19:32:11 -0400

    calculator calculate test

--- test/calculate-test.js
+++ test/calculate-test.js
@@ -10,4 +10,8 @@ const should = require('should');
             let calc = new Calculator();
             calc.calculate('1 2 +').should.equal(3);
         });
+        it.skip('monomial operation returns answer', function () {
+            let calc = new Calculator();
+            calc.calculate('1 double').should.equal(2);
+        });
     });
