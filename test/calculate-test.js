6af365c51cf2dedd32357b5ef9fe7203497dc65c
Jason Ray <jasonray@jayray.net>
2019-10-03 20:07:37 -0400

    implement calculate

--- test/calculate-test.js
+++ test/calculate-test.js
@@ -6,9 +6,10 @@ const assert = require('assert');
 const should = require('should');
 
     describe('calculate', function () {
-        it.skip('binary operation returns answer', function () {
+        it('binary operation returns answer', function () {
             let calc = new Calculator();
-            calc.calculate('1 2 +').should.equal(3);
+            let result = calc.calculate('1 2 +');
+            result.should.equal(3);
         });
         it.skip('monomial operation returns answer', function () {
             let calc = new Calculator();
